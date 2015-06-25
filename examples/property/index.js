c949b45a26224774b2aba8ed68a37122bbed2222
Jim de Beer <jim@vigour.io>
2015-06-25 15:26:06 +0200

    property event uses $postpone, fires from setKeyInteral

--- examples/property/index.js
+++ examples/property/index.js
@@ -5,3 +5,21 @@ var log = require('../../lib/dev/log')
 var Base = require('../../lib/base')
 
 log('test property event type')
+
+var a = new Base({
+  $key:'a',
+  bla:true,
+  $on: {
+    $change:function(e) {
+
+    },
+    $property: function(e) {
+      //may need to add extra property info!
+      log('hello property update!')
+    }
+  }
+})
+
+a.$set({
+  b:1
+})
