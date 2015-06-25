572fe0bd068280024631792ff634b279e2e099d5
Jim de Beer <jim@vigour.io>
2015-06-25 15:50:06 +0200

    added $property emitter type, added $meta dispatcher object

--- examples/property/index.js
+++ examples/property/index.js
@@ -10,16 +10,24 @@ var a = new Base({
   $key:'a',
   bla:true,
   $on: {
-    $change:function(e) {
-
+    $change:function(event) {
+      log('hello change')
     },
-    $property: function(e) {
+    $property: function(event, meta) {
       //may need to add extra property info!
-      log('hello property update!')
+      log('hello property update!', JSON.stringify(meta))
     }
   }
 })
 
 a.$set({
   b:1
+})
+
+
+a.$set({
+  b:1,
+  c:2,
+  d:3,
+  g:5
 })
