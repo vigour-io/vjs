a0475597837851e515163d5ae9b7739e7f192f95
Jim de Beer <jim@vigour.io>
2015-06-26 13:49:19 +0200

    renamed _$operators to $operators

--- examples/property/index.js
+++ examples/property/index.js
@@ -14,12 +14,12 @@ var a = new burk.$Constructor({
   $key:'a',
   bla:true,
   $on: {
-    // $change:function(event) {
-      // log('hello change', event)
-    // },
+    $change:function(event) {
+      log('a hello change', this.$path)
+    },
     $property: function(event, meta) {
       //may need to add extra property info!
-      log('hello property update!', JSON.stringify(meta))
+      log('hello property update!', this.$path, JSON.stringify(meta))
     },
     $new:function(event) {
       log('NEW hello! NEW', event )
@@ -32,37 +32,71 @@ var a = new burk.$Constructor({
 log('\n\n\n\if fired fail!')
 
 // console.clear()
-log('once-----')
+log('once----- a')
 
 a.$set({
   b:1
 })
 
-log('twice')
+// log('twice')
 
-a.$set({
-  b:1,
-  c:2,
-  d:3,
-  g:5
-})
+// a.$set({
+//   b:1,
+//   c:2,
+//   d:3,
+//   g:5
+// })
 
-log('newxxxx')
+log('new b log property -b')
 
 var b = new a.$Constructor({
   $key:'b',
-  ggg:true
-})
-
-log('newxxxx2')
-
-var c = new b.$Constructor({
-  $key:'c',
   ggg:true,
-  gurk:true,
-  myblurf:true,
-  murko:true
+  $on: {
+    //what goes wrong? listeners are completely overwriten
+    //change will nog fire a anymore
+    $change:function(event) {
+      log('b hello change', this.$path)
+    },
+    $property:function(event, meta) {
+      log('hey property but only on b and inheritance of b', this.$path, JSON.stringify(meta), event)
+    }
+  }
 })
+
+log('property update on b')
+
+b.$set({
+  blurx:'x'
+})
+
+log('fire both --')
+
+a.$set({
+  blury:'y'
+})
+
+log('only a is wrong / or perhaps use instances ?')
+
+
+log('new instance of b')
+
+var flurps = new b.$Constructor({
+  $key:'flurps',
+  flurps:true
+})
+
+log('should also fire on a listener (inherit?)')
+
+// log('newxxxx2')
+
+// var c = new b.$Constructor({
+//   $key:'c',
+//   ggg:true,
+//   gurk:true,
+//   myblurf:true,
+//   murko:true
+// })
 
 // a.$emit('$change')
 
