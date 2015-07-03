251727edb6d8e7ee7c3df8d22f304c75e566d1b2
marcusbesjes <marcusbesjes@gmail.com>
2015-07-03 17:13:02 +0200

    first version of list

--- examples/list/index.js
+++ examples/list/index.js
@@ -10,84 +10,9 @@ var Krek = require('./krek')
 
 var define = Object.defineProperty
 
-// var first = new Base({
-//   key1: 1
-// })
-// first._$key = 'first'
+//-------------------------------------------------------------
 
-
-// var second = new first.$Constructor({
-//   key2: 2
-// })
-// second._$key = 'second'
-
-
-// console.log('?', second.key1.$path)
-
-// var arr = [new Base('A'), new Base('A')]
-
-// arr.unshift(new Base('B'))
-
-// for(var i in arr) {
-//   console.log('i', arr[i])
-// }
-
-
-// var burk = {}
-
-// define(burk, 'unshift', {
-//   value: Array.prototype.unshift
-// })
-
-// burk.normalkey1 = 1
-
-// define(burk, 'getterkey2', {
-//   get: function() {
-//     console.log('GET getterkey2')
-//     return new Base('???')
-//   },
-//   set: function() {
-//     console.log('SET getterkey2')
-//   },
-//   enumerable: true
-// })
-
-// burk.unshift('????')
-
-// for(var f in burk) {
-//   console.log('FIELD', f)
-// }
-
-
-
-
-
-
-
-var Shurk = function Shurk() {
-  this.shurk = true
-}
-
-// console.log('?!', Shurk.prototype)
-
-// Shurk.prototype.__proto__ = new Array()
-
-// var shurk = new Shurk()
-
-// console.log('shurk shurk instanceof Shurk', shurk instanceof Shurk)
-// console.log('shurk shurk instanceof Array', shurk instanceof Array)
-
-// shurk.unshift('hehehaha')
-
-// console.log('shurk:', shurk)
-
-// for(var i in shurk) {
-//   console.log('key in shurk', i, shurk[i])
-// }
-
-// throw 'stop!'
-
-
+var $getPropertyValue = require('../../lib/base/set').$getPropertyValue
 
 
 //-------------------------------------------------------------
@@ -105,8 +30,6 @@ define(list, '$push', {
   }
 })
 
-var STATUS = 0
-
 define(list, '_A_unshift', {
   value: Array.prototype.unshift
 })
@@ -123,13 +46,7 @@ define(list, '$unshift', {
 
     var length = this.length
 
-    if(STATUS === 1) {
-      console.log('--- about to unshift into', this._$key)
-      this.$each(function(value, key){
-        console.log('--- key', key)
-      })
-    }
-
+    console.error('<<<<<<<<<<<<< unshift!', arguments)
     this._A_unshift.apply(this, arguments)
 
     var newLength = this.length
@@ -141,25 +58,34 @@ define(list, '$unshift', {
 
       var newValsOldKeys = Math.min(length, newValues)
       while(i < newValsOldKeys) {
-        console.log('new value in old key bur!', i)
+        console.warn('new value in old key bur!', i)
+        this[i] = $getPropertyValue(this[i], event, this, i)
         i++
-
-        // this[i] = new this.$ChildConstructor( this[i], event, this, i )
-
-
       }
 
       var oldValsOldKeys = length - newValues
       if(oldValsOldKeys) {
         if(oldValsOldKeys > 0) {
           while(oldValsOldKeys) {
-            console.log('old value in old key!', i)
+            console.warn('old value in old key!', i)
+
+            var item = this[i]
+            if(item._$parent === this) {
+              console.log('i am the parent')
+              item._$key = i
+            } else {
+              console.log('oh noes I am context bay')
+              this.$createListContextGetter(i)
+            }
+
             i++
             oldValsOldKeys--
           }
         } else {
           while(oldValsOldKeys) {
-            console.log('new value in new key!', i)
+            console.warn('new value in new key!', i)
+
+            this[i] = $getPropertyValue(this[i], event, this, i)
             i++
             oldValsOldKeys++
           }
@@ -167,49 +93,36 @@ define(list, '$unshift', {
       }
 
       while(i < newLength) {
-        console.log('old value in new key', i)
+        console.warn('old value in new key', i, this._$key)
+        var item = this[i]
+
+        console.log('look at item!', item)
+
+        if(item._$parent === this) {
+          console.log('i am the parent')
+          item._$key = i
+        } else {
+          console.log('oh noes I am context bay')
+          this.$createListContextGetter(i)
+        }
+        // this[i]._$key = i
+
+
+        // if(this.hasOwnProperty( '_$Constructor' )) {
+        //   console.error('dodat createContextGetter')
+        //   this.$createContextGetter.call( this, String(i) )
+        // }
         i++
       }
 
     } else {
-      console.log('all new values in all new keys!')
-
-
+      console.warn('all new values in all new keys!')
       while(i < newValues) {
-
-
-        // console.log('?E', this.hasOwnProperty( '_$Constructor' ))
-
-        // this[i] = {lurkwex: 1}
-        console.log('SET DAT KEY!!!')
-        var childburk = new this.$ChildConstructor( this[i], event, this, i )
-
-        console.log('?!?!?! WEXEEEE', i)
-
-        this[i] = new Base(1)
-
-        // this.$setKeyInternal(i, this[i])
-
+        this.$setKeyInternal(i, this[i])
         i++
       }
     }
     
-
-
-    // for(var i = 0 ; i < newValues ; i++) {
-    //   // var value = this[i]
-    //   console.log('unshifted!', i, this[i])
-    //   this.$setKeyInternal(i, this[i])
-    // }
-    // if(length) {
-
-    //   var 
-
-    // }
-    // for(; i < length ; i++) {
-    //   console.log('this field was there')  
-    // }
-
   }
 })
 
@@ -233,6 +146,7 @@ list.$unshift('addedFirstA')
 console.log('-------------- list:')
 list.$each(function(val, key){
   console.log(key, ':', val)
+  console.log('path:', val.$path)
 })
 console.log('--------------')
 
@@ -240,13 +154,16 @@ console.log('\n\n\n\n---------- make listinstance')
 var listinstance = window.listinstance = new list.$Constructor()
 listinstance._$key = 'listinstance'
 
-STATUS = 1
+console.log('urrr', list[0].$parent, list[0]._$key, list[0].$key)
+console.log('listinstancepath', listinstance[0].$path)
+
 console.log('=================== listinstance.$unshift(\'shagsworn\')')
 listinstance.$unshift('unshiftedthisB')
 
 console.log('-------------- listinstance:')
 listinstance.$each(function(val, key){
   console.log(key, ':', val)
+  console.log('path:', val.$path)
 })
 console.log('--------------')
 
@@ -255,56 +172,32 @@ console.log('\n\n\n\n=================== look at dem!')
 console.log('-------------- list:')
 list.$each(function(val, key){
   console.log(key, ':', val)
+  console.log('path:', val.$path)
 })
 console.log('--------------')
 
 console.log('-------------- listinstance:')
 listinstance.$each(function(val, key){
   console.log(key, ':', val)
+  console.log('path:', val.$path)
 })
-
-listinstance[111]=1
-listinstance[12]=1
-
-
-listinstance['lep']=1
-define(listinstance, 'swex', {
-  get: function() {
-
-  },
-  set: function() {
-
-  },
-  enumerable: true
-})
-listinstance.zep = 1
-listinstance.aep = 1
-
-
-
-
-for(var f in listinstance) {
-  console.log('KEY:', f)
-}
-
 console.log('--------------')
 
+listinstance.$unshift('C1', 'C2', 'C3')
 
-// list.$$unshift('shine')
-// list.$$unshift('durk', 'smurk', 'sjeisk')
+console.log('-------------- listinstance:')
+listinstance.$each(function(val, key){
+  console.log(key, ':', val)
+  console.log('path:', val.$path)
+})
+console.log('--------------')
 
-// list.$$splice(2,0,'GESPLICED')
+listinstance.$unshift('D1')
 
-// console.log('-------------- list:')
-// list.$each(function(val, key){
-//   console.log(key, ':', val)
-// })
-// console.log('--------------')
+console.log('-------------- listinstance:')
+listinstance.$each(function(val, key){
+  console.log(key, ':', val)
+  console.log('path:', val.$path)
+})
+console.log('--------------')
 
-// list.$$ArraySort()
-
-// console.log('-------------- list:')
-// list.$each(function(val, key){
-//   console.log(key, ':', val)
-// })
-// console.log('--------------')
