732692b14381091b3ce5df80e7281e298aa57097
marcusbesjes <marcusbesjes@gmail.com>
2015-07-06 09:29:44 +0200

    list

--- examples/list/index.js
+++ examples/list/index.js
@@ -27,7 +27,7 @@ define(list, '_A_push', {
 define(list, '$push', {
   value: function $push() {
 
-    event = new Event()
+    var event = new Event()
     event.$val = arguments
     event.$origin = this
 
@@ -41,7 +41,15 @@ define(list, '$push', {
     // var newLength = this.length
     // var newValues = newLength - length
 
-    var i = 0
+    var i = length
+    while(newValues) {
+      console.warn('new value new key', i)
+
+      this.$setKeyInternal(i, this[i])
+      newValues--
+      i++
+    }
+
 
   }
 })
@@ -52,7 +60,7 @@ define(list, '_A_unshift', {
 define(list, '$unshift', {
   value: function $unshift(){
 
-    event = new Event()
+    var event = new Event()
     event.$val = arguments
     event.$origin = this
 
@@ -120,8 +128,6 @@ define(list, '$unshift', {
           this.$createListContextGetter(i)
         }
         // this[i]._$key = i
-
-
         // if(this.hasOwnProperty( '_$Constructor' )) {
         //   console.error('dodat createContextGetter')
         //   this.$createContextGetter.call( this, String(i) )
@@ -143,6 +149,105 @@ define(list, '$unshift', {
 define(list, '_A_splice', {
   value: Array.prototype.splice
 })
+define(list, '$splice', {
+  value: function $splice(start, remove) {
+    if(!remove) remove = 0
+
+    var event = new Event()
+    event.$val = arguments
+    event.$origin = this
+
+    var ret
+    if(remove) {
+      if(remove === 1) {
+        ret = [this[start]]
+      } else {
+        ret = []
+        for(var i = start, n = remove ; n ; n-- && i++ ) {
+          ret.push(this[i])
+        }
+      }
+    }
+
+    var newValues = arguments.length - 2
+    var length = this.length
+    
+    console.error('<<>><<>><<>>< splice', arguments, arguments.length)
+    this._A_splice.apply(this, arguments)
+
+    var i = Math.min(start, length)
+
+    var newValsOldKeys = Math.min(newValues, length-start)
+
+    while(newValsOldKeys) {
+      console.warn('new value in old key', i)
+      this[i] = $getPropertyValue(this[i], event, this, i)
+      i++
+      newValsOldKeys--
+    }
+
+    var newValsNewKeys = start + newValues - length
+
+    if(newValsNewKeys > 0) {
+      while(newValsNewKeys) {
+        console.warn('new value in new key', i)
+        this[i] = $getPropertyValue(this[i], event, this, i)
+        i++
+        newValsNewKeys--
+      }
+    } else if(newValues > remove){
+      
+    }
+      var oldValsOldKeys = -newValsNewKeys - remove
+      newValsNewKeys = 0
+      console.log('oldValsOldKeys',oldValsOldKeys, newValsNewKeys, oldValsOldKeys, remove)
+      while(oldValsOldKeys) {
+        console.warn('old value in old key', i)
+        var item = this[i]
+        if(item._$parent === this) {
+          console.log('i am the parent')
+          item._$key = i
+        } else {
+          console.log('oh noes I am context bay')
+          this.$createListContextGetter(i)
+        }
+        i++
+        oldValsOldKeys--
+      }
+        
+
+    }
+
+    if(length) {
+      var oldValsNewKeys = newValues - newValsNewKeys - remove
+      if(oldValsNewKeys > 0) {
+        while(oldValsNewKeys) {
+          console.warn('old value in new key', i)
+          var item = this[i]
+          if(item._$parent === this) {
+            console.log('i am the parent')
+            item._$key = i
+          } else {
+            console.log('oh noes I am context bay')
+            this.$createListContextGetter(i)
+          }
+          i++
+          oldValsNewKeys--
+        }  
+      }
+    }
+
+    if(ret) {
+      for(var i = 0, l = ret.length ; i < l ; i++) {
+        // ret[i].remove(event)
+        console.warn('remove!')
+      }
+      return ret
+    }
+
+  }
+})
+
 
 define(list, '_A_sort', {
   value: Array.prototype.sort
@@ -215,3 +320,22 @@ listinstance.$each(function(val, key){
 })
 console.log('--------------')
 
+
+listinstance.$push('PUSH')
+
+console.log('-------------- listinstance:')
+listinstance.$each(function(val, key){
+  console.log(key, ':', val)
+  console.log('path:', val.$path)
+})
+console.log('--------------')
+
+
+listinstance.$splice(2,1,'SPLICED!')
+
+console.log('-------------- listinstance:')
+listinstance.$each(function(val, key){
+  console.log(key, ':', val)
+  console.log('path:', val.$path)
+})
+console.log('--------------')
