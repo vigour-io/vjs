0879220b364018a3db526ae9aae7f5e43f9fd9ab
marcusbesjes <marcusbesjes@gmail.com>
2015-07-06 21:12:48 +0200

    listing

--- examples/list/splice_notes.js
+++ examples/list/splice_notes.js
@@ -51,13 +51,18 @@ define(list, '$splice', {
       }
     }
 
+    // console.error('<<>><<>><<>>< splice', arguments, arguments.length)
+    // console.log('this', this.$toString())
     _A_splice.apply(this, arguments)
 
-    var i = Math.min(start, length)
+    // console.log('wext', this)
 
+    var i = Math.min(start, length)
+    // console.log('ik ga beginnen bij', i)
     var newValsOldKeys = Math.min(newValues, length-start)
 
     while(newValsOldKeys) {
+      // console.warn('new value in old key', i)
       this[i] = $getPropertyValue(this[i], event, this, i)
       i++
       newValsOldKeys--
@@ -65,9 +70,15 @@ define(list, '$splice', {
 
     var newValsNewKeys = start + newValues - length
 
+    // console.log('newValsNewKeys', newValsNewKeys)
+    // console.log('start', start)
+    // console.log('newValsOldKeys', newValsOldKeys)
+    // console.log('length', length)
+
     if(newValsNewKeys > 0) {
       var j = newValsNewKeys
       while(j) {
+        // console.warn('new value in new key', i)
         this[i] = $getPropertyValue(this[i], event, this, i)
         i++
         j--
@@ -76,11 +87,22 @@ define(list, '$splice', {
 
       var oldValsOldKeys = -newValsNewKeys - remove
       newValsNewKeys = 0
+      // console.log('oldValsOldKeys',oldValsOldKeys, newValsNewKeys, oldValsOldKeys, remove)
 
       var j = oldValsOldKeys
 
       while(j) {
-        $handleShifted(this, i)
+        // console.warn('old value in old key', i)
+        var item = this[i]
+
+        if(item._$parent === this) {
+          // console.log('i am the parent')
+          item._$key = i
+        } else {
+          // console.log('oh noes I am context bay')
+          this.$createListContextGetter(i)
+        }
+        i++
         j--
       }
 
@@ -90,9 +112,24 @@ define(list, '$splice', {
     if(length && start < length) {
       var oldValsNewKeys = newValues - newValsNewKeys - remove
 
+      // console.log('oldValsNewKeys', oldValsNewKeys)
+      // console.log('newValues', newValues)
+      // console.log('newValsNewKeys', newValsNewKeys)
+      // console.log('remove', remove)
+
       if(oldValsNewKeys > 0) {
         while(oldValsNewKeys) {
-          $handleShifted(this, i)
+          // console.warn('old value in new key', i, this)
+
+          var item = this[i]
+
+          if(item._$parent === this) {
+            // console.log('i am the parent')
+            item._$key = i
+          } else {
+            // console.log('oh noes I am context bay')
+            this.$createListContextGetter(i)
+          }
           i++
           oldValsNewKeys--
         }  
@@ -120,11 +157,11 @@ if(!remove) remove = 0
     event.$origin = this
 
     var newValues = arguments.length - 2
-    var oldLength = this.length
+    var length = this.length
 
     var ret
     if(remove) {
-      var maxrem = oldLength - start
+      var maxrem = length - start
       if(remove > maxrem) {
         remove = maxrem
       }
@@ -146,92 +183,83 @@ if(!remove) remove = 0
 
     // console.log('wext', this)
 
-    var length = this.length
+    var i = Math.min(start, length)
+    // console.log('ik ga beginnen bij', i)
+    var newValsOldKeys = Math.min(newValues, length-start)
 
-    var newValsOldKeys = Math.min(newValues, oldLength-start)
-    var newValsMaxKey = start + newValues
-    var olValsMaxKey = oldLength - start - remove
-
-    for(var i = Math.min(start, oldLength) ; i < length ; i++) {
-      if(i < newValsMaxKey) {
-        // console.log('new value!', this[i], i)
-        this[i] = $getPropertyValue(this[i], event, this, i)
-        // console.log('new value!', this[i])
-      } else{
-        $handleShifted(this, i)
-      }
-
-    }
-
-    if(ret) {
-      for(var i = 0, l = ret.length ; i < l ; i++) {
-        // ret[i].remove(event)
-        // console.warn('remove!')
-      }
-      return ret
-    }
-
-  }
-})
-
-
-define(list, '$splice3', {
-  value: function $splice(start, remove) {
-if(!remove) remove = 0
-
-    var event = new Event()
-    event.$val = arguments
-    event.$origin = this
-
-    var newValues = arguments.length - 2
-    var oldLength = this.length
-
-    var ret
-    if(remove) {
-      var maxrem = oldLength - start
-      if(remove > maxrem) {
-        remove = maxrem
-      }
-
-      if(remove === 1) {
-        ret = [this[start]]
-      } else {
-        ret = []
-        for(var i = start, n = remove ; n ; n-- && i++ ) {
-          var item = this[i]
-          ret.push(this[i])
-        }
-      }
-    }
-
-    // console.error('<<>><<>><<>>< splice', arguments, arguments.length)
-    // console.log('this', this.$toString())
-    _A_splice.apply(this, arguments)
-
-    // console.log('wext', this)
-
-    var length = this.length
-
-    var newValsOldKeys = Math.min(newValues, oldLength-start)
-    var newValsMaxKey = start + newValues
-    var olValsMaxKey = oldLength - start - remove
-
-    var i = Math.min(start, oldLength)
-
-    while(i < newValsMaxKey) {
+    while(newValsOldKeys) {
+      // console.warn('new value in old key', i)
       this[i] = $getPropertyValue(this[i], event, this, i)
+      i++
+      newValsOldKeys--
     }
-    
 
-    for(var i = Math.min(start, oldLength) ; i < length ; i++) {
-      if(i < newValsMaxKey) {
-        // console.log('new value!', this[i], i)
+    var newValsNewKeys = start + newValues - length
+
+    // console.log('newValsNewKeys', newValsNewKeys)
+    // console.log('start', start)
+    // console.log('newValsOldKeys', newValsOldKeys)
+    // console.log('length', length)
+
+    if(newValsNewKeys > 0) {
+      var j = newValsNewKeys
+      while(j) {
+        // console.warn('new value in new key', i)
         this[i] = $getPropertyValue(this[i], event, this, i)
-        // console.log('new value!', this[i])
-      } else{
-        $handleShifted(this, i)
+        i++
+        j--
+      }
+    } else if(newValues < remove){
+
+      var oldValsOldKeys = -newValsNewKeys - remove
+      newValsNewKeys = 0
+      // console.log('oldValsOldKeys',oldValsOldKeys, newValsNewKeys, oldValsOldKeys, remove)
+
+      var j = oldValsOldKeys
+
+      while(j) {
+        // console.warn('old value in old key', i)
+        var item = this[i]
+
+        if(item._$parent === this) {
+          // console.log('i am the parent')
+          item._$key = i
+        } else {
+          // console.log('oh noes I am context bay')
+          this.$createListContextGetter(i)
+        }
+        i++
+        j--
       }
 
+    }
+
+
+    if(length && start < length) {
+      var oldValsNewKeys = newValues - newValsNewKeys - remove
+
+      // console.log('oldValsNewKeys', oldValsNewKeys)
+      // console.log('newValues', newValues)
+      // console.log('newValsNewKeys', newValsNewKeys)
+      // console.log('remove', remove)
+
+      if(oldValsNewKeys > 0) {
+        while(oldValsNewKeys) {
+          // console.warn('old value in new key', i, this)
+
+          var item = this[i]
+
+          if(item._$parent === this) {
+            // console.log('i am the parent')
+            item._$key = i
+          } else {
+            // console.log('oh noes I am context bay')
+            this.$createListContextGetter(i)
+          }
+          i++
+          oldValsNewKeys--
+        }  
+      }
     }
 
     if(ret) {
@@ -244,6 +272,7 @@ if(!remove) remove = 0
 
   }
 })
+
 
 define(list, '_A_sort', {
   value: Array.prototype.sort
@@ -290,7 +319,7 @@ list2.$each(function(value, key){
 
 // throw 'stop'
 
-var n = 10e3
+var n = 10
 
 // var l5 = new list.$Constructor()
 
