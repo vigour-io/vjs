2ec67aba2a780ecd073fd92950186fef499d132d
Jim de Beer <jim@vigour.io>
2015-07-14 20:05:31 +0200

    uncommented example

--- examples/on-advanced/index.js
+++ examples/on-advanced/index.js
@@ -6,96 +6,97 @@ var log = require('../../lib/dev/log')
 
 var Base = require('../../lib/base')
 
-// var burk = new Base()
+var burk = new Base()
 
-// var a = new burk.$Constructor({
-//   $key:'a',
-//   bla:true,
-//   $on: {
-//     $change:function(event) {
-//       log('a hello change', this.$path)
-//     }
-//     //this has to put it in $val
-//   }
-// })
+var a = new burk.$Constructor({
+  $key:'a',
+  bla:true,
+  $on: {
+    $change:function(event) {
+      log('a hello change', this.$path)
+    }
+    //this has to put it in $val
+  }
+})
 
-// log('a:',a)
+log('a:',a)
 
-// console.log(a.$on.$change)
+console.log(a.$on.$change)
 
-// a.$val = 'marcus'
+a.$val = 'marcus'
 
-// //-------------------------------------------------------------
+//-------------------------------------------------------------
 
-// log('fire a -- with b (new instance)')
+log('fire a -- with b (new instance) WRONG!')
 
-// var b = new a.$Constructor({
-//   $key:'b',
-//   $val:'marcus2'
-// })
+var b = new a.$Constructor({
+  $key:'b',
+  $val:'marcus2'
+})
+//deze is nog helemaal wrong bitches!
 
 
-// log('make c dont fire!')
+log('make c dont fire!')
 
-// var c = new a.$Constructor({
-//   $key:'c',
-//   $on: {
-//     $change:function(event) {
-//       log('c hello change', this.$path)
-//     }
-//   },
-//   $val:'marcus3'
-// })
+var c = new a.$Constructor({
+  $key:'c',
+  $on: {
+    $change:function(event) {
+      log('c hello change', this.$path)
+    }
+  },
+  $val:'marcus3'
+})
 
-// console.log(
-//   'a:',a.$on.$change, 
-//   'c:',c.$on.$change, 
-//   'c instanceof a:', c.$on.$change instanceof a.$on.$change.$Constructor
-// )
+console.log(
+  'a:',a.$on.$change, 
+  'c:',c.$on.$change, 
+  'c instanceof a:', c.$on.$change instanceof a.$on.$change.$Constructor
+)
 
-// log('now fire c')
+log('now fire c')
 
-// c.$val = 'marcus'
+c.$val = 'marcus'
 
-// //-------------------------------------------------------------
+//-------------------------------------------------------------
 
-// log('----object notation---')
-// console.log('\n\n object')
+log('----object notation---')
+console.log('\n\n object')
 
-// var gurken = new Base({
-//   $key:'gurken',
-//   $on: {
-//     $change: {
-//       marcus: function() {
-//         log('marcus fires', this.$path)
-//       },
-//       jim: function() {
-//         log('jim fires', this.$path)
-//       }
-//     }
-//   }
-// })
+var gurken = new Base({
+  $key:'gurken',
+  $on: {
+    $change: {
+      marcus: function() {
+        log('marcus fires', this.$path)
+      },
+      jim: function() {
+        log('jim fires', this.$path)
+      }
+    }
+  }
+})
 
-// gurken.$val = 'clubs'
-// console.log(gurken.$on.$change)
+gurken.$val = 'clubs'
+console.log(gurken.$on.$change)
 
-// log('----new instance, only change jim--- should fire marcus since its not new...')
+log('----new instance, only change jim--- should fire marcus since its not new...')
 
-// var blurken = new gurken.$Constructor({
-//   $key:'blurken',
-//   $on: {
-//     $change: {
-//       jim:function() {
-//         log('jim 2 fires (only for blurken', this.$path)
-//       }
-//     }
-//   },
-//   $val:'boeloe'
-// })
+var blurken = new gurken.$Constructor({
+  $key:'blurken',
+  $on: {
+    $change: {
+      jim:function() {
+        log('jim 2 fires (only for blurken', this.$path)
+      }
+    }
+  },
+  $val:'boeloe'
+})
 
-// log('----fire both---')
+log('----fire both---')
 
-// blurken.$val = 'boeloe2'
+blurken.$val = 'boeloe2'
 
 log('----refs----')
 
@@ -162,44 +163,44 @@ console.log( glurps.$on._instances )
 
 console.log('LISTENS?', glurps)
 
-// log('----bound----')
+log('----bound----')
 
-// var blups = new Base({
-//   $key: 'blups',
-//   $val: 'this is blups!'
-// })
+var blups = new Base({
+  $key: 'blups',
+  $val: 'this is blups!'
+})
 
-// var blups2 = new Base({
-//   $key:'blups2',
-//   $on: {
-//     $change: {
-//       bindit: [ 
-//         function( event, meta, base, str, str2 ) {
-//           //this is blups
-//           //when blups gets removed will remove this listener
-//           // str = gurken
-//           // str2 = gurken
-//           log(
-//             'OK I CHANGE THE BINDIT!', 
-//             'bound2:', 
-//              this.$path, 
-//              base.$path,
-//              'event:', 
-//              event, 
-//              'args:', 
-//              str, 
-//              str2 
-//            )
-//         }, 
-//         blups, 
-//         'gurken', 
-//         'marcus' 
-//       ]
-//     }
-//   }
-// })
+var blups2 = new Base({
+  $key:'blups2',
+  $on: {
+    $change: {
+      bindit: [ 
+        function( event, meta, base, str, str2 ) {
+          //this is blups
+          //when blups gets removed will remove this listener
+          // str = gurken
+          // str2 = gurken
+          log(
+            'OK I CHANGE THE BINDIT!', 
+            'bound2:', 
+             this.$path, 
+             base.$path,
+             'event:', 
+             event, 
+             'args:', 
+             str, 
+             str2 
+           )
+        }, 
+        blups, 
+        'gurken', 
+        'marcus' 
+      ]
+    }
+  }
+})
 
-// blups2.$val = 'xxxxx'
+blups2.$val = 'xxxxx'
 
 } catch(e) {
   console.error(e.stack)
