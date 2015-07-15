b5f93a1d0b6b0c1c60f75296468baa64e1e54cbb
marcusbesjes <marcusbesjes@gmail.com>
2015-07-15 22:47:20 +0200

    working on results

--- examples/add/index.js
+++ examples/add/index.js
@@ -7,15 +7,48 @@ var Base = require('../../lib/base')
 
 // =============================================================
 
-var base = new Base({
-  field: 'field',
-  $add:{
-    addedfield: 'addedfield'
-  }
-})
 
+var setobj = [
+  {id: 0, name: 'durk', age: 12},
+  {id: 1, name: 'ddurk', age: 122},
+  {id: 2, name: 'larf', age: 102},
+  {id: 3, name: 'smark', age: 1},
+]
+setobj.$sort = 'age'
+setobj.$add = [{id:4, name:'ADDED', age: 103}]
+
+
+var base = new Base(setobj)
+base._$key = 'base'
 
 console.log('\n--------- get val 1')
-var $val = base.$val
+var $val = window.$val = base.$val
 
-console.log('$val:', $val)
+console.log('$val:', raw($val))
+
+console.log('\n--------- now change something sorted!')
+base[0].age.$val = 150
+console.log('base.$sort._results:', raw(base.$sort._results))
+console.log('$val:', raw($val))
+
+
+
+
+
+function raw(base) {
+  base = base.$origin
+  var result
+  base.$each(function(value, key){
+    if(!result) {
+      result = {}
+      if(base._$val !== undefined) {
+        result.$val = base._$val
+      }
+    }
+    result[key] = raw(value)
+  })
+  if(!result) {
+    result = base._$val
+  }
+  return result
+}
