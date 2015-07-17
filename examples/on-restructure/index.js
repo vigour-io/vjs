475b2957a3e6f4d808950a6ac205e0987b70fa83
Jim de Beer <jim@vigour.io>
2015-07-17 16:52:06 +0200

    added uid (injectable in base) to create something resembleing weakmaps, redid emitter and emit (allmost complete for instances) #25

--- examples/on-restructure/index.js
+++ examples/on-restructure/index.js
@@ -21,7 +21,7 @@ var obs = new Observable({
   $on: {
     $change: function( event, meta ) {
       //why does getting $key not work?
-      log( 'val change listener (from obs)', this.$path, event )
+      log( 'val change listener (from obs)', this.$path )
     },
     $property: function( event, meta ) {
       log('val property listener (from obs)', JSON.stringify(meta), this.$path)
@@ -35,7 +35,7 @@ obs.$set({
   x:true
 })
 
-log('--- new instance - now log val (from obs) listener and property (from obs)------')
+log('--- new instance - now log val (from obs) listener and property (from obs context bla)------')
 
 var bla = new obs.$Constructor({
   $key:'bla',
@@ -48,7 +48,123 @@ var bla = new obs.$Constructor({
     }
   }
 })
-//why does property fire and not the other?
 
-log('------')
+log('---references tests---')
 
+var a = new Observable({
+  $key:'a',
+  $val: 10
+})
+
+var b = new Observable({
+  $key:'b',
+  $val: 10,
+  $add: 10
+})
+
+console.warn('make c')
+
+var c = new Observable({
+  $key:'c',
+  $val: a,
+  $add: b,
+  $on: {
+    $change: function( event, meta ) {
+      // console.log('CHANGE!', this)
+      log('change from c', this.$path, this.$val)
+    },
+    // $new: function( event, meta ) {
+    //   // log( 'new instance!', 'current instance:', this.$path, 'from:', meta.$path )
+    // }
+  }
+})
+
+log(c.$val) 
+
+log('-- update a---- (c should update)')
+
+// console.clear()
+
+a.$val = 20
+
+// debugger
+// console.clear()
+
+log('--d new instance of c expect c to fire (context d) (since set /w key)')
+// //think about this maybe key should not update?
+
+console.warn('make d')
+
+// //nu word d2x gecalled omdat bound niet word beijgehouden in dingen die geexclude moeten
+// //worden mischien toch in de emitter?
+// //anders is er geen difference...
+
+
+//   setup
+//   bind word een array
+//   word altijd meegenomen in stamp
+//   $emit in emitter is responsible voor het uitvoeren van multiple bound things
+//   (dus exec met different bound)
+
+//   __bind__ is altijd array ( for now )
+
+//   emitter emit doet exec op multiple --- $emit van obs is wel responsible voor het added?
+
+//   //ook niet called alleen emit nog een keer!
+
+
+
+
+try {
+
+
+//use uid system for this
+//since d has and update from c and one of its own
+//this will break it
+
+var d = new c.$Constructor({
+  $key:'d',
+  $on: {
+    $change: {
+      //now this is unique so it will work
+      flups:function(){
+        //moet nog checken of een instance eigen onFn heeft
+        log('flups', this.$path)
+      }
+    }
+  }
+})
+
+} catch(e) {
+  console.error(e.stack)
+}
+
+// console.clear()
+log('update a, expect d and c to fire, fulps as well (context d)')
+a.$val = 30
+
+log('--e new instance of d, fire c context e, flups (context e)')
+
+console.warn('make e')
+
+var e = new d.$Constructor({
+  $key:'e',
+  $add:100
+})
+
+
+log('--- change a ref fire c context c,d,e ---, flups (context e,d)')
+
+a.$val = 40
+
+
+log('--- change b ref fire c context c,d, NO e ---, flups (context e,d)')
+
+b.$val = 40
+
+
+
+
+
+//test overwriting the instance
+//test base (not doing instances)
