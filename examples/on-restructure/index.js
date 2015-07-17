12bd610c167fb9fd491ea37b28feb78670ed91dc
Jim de Beer <jim@vigour.io>
2015-07-17 12:38:39 +0200

    added passon in each, revert logic of return values of exclude function in each, added logic to block listeners when you set them #25

--- examples/on-restructure/index.js
+++ examples/on-restructure/index.js
@@ -3,7 +3,9 @@ var perf = require('../../dev/perf')
 //-----------------------------------------------------
 
 var Base = require('../../lib/base')
+
 Base.prototype.inject( require('../../lib/methods/toString') )
+//wil eigenlijk ook op constructors kunnen injecten
 
 var Observable = require('../../lib/observable')
 
@@ -19,29 +21,34 @@ var obs = new Observable({
   $on: {
     $change: function( event, meta ) {
       //why does getting $key not work?
-      log( 'fire obs event', this.$path )
+      log( 'val change listener (from obs)', this.$path, event )
     },
     $property: function( event, meta ) {
-      log('fire prop:', JSON.stringify(meta), this.$path)
+      log('val property listener (from obs)', JSON.stringify(meta), this.$path)
     }
   }
 })
 
+log('--- now log change and property ------')
+
 obs.$set({
   x:true
 })
+
+log('--- new instance - now log val (from obs) listener and property (from obs)------')
 
 var bla = new obs.$Constructor({
   $key:'bla',
   marcus:'besjes',
   $on: {
     $change: {
-      mups: function( event, meta ) {
-        log('wtf wtf wtf', event)
+      speshField: function( event, meta ) {
+        log('speshField change listener (from bla)', event)
       }
     }
   }
 })
+//why does property fire and not the other?
 
 log('------')
 
