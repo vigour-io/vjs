dfba161eff0229e0179692fc38e291c3ff37cf86
Jim de Beer <jim@vigour.io>
2015-07-17 17:15:17 +0200

    fixed a bug in protection for updates when setting $on whe using .$set

--- examples/on-restructure/index.js
+++ examples/on-restructure/index.js
@@ -16,151 +16,178 @@ Observable.prototype.inject(
   require('../../lib/operator/multiply')
 )
 
-var obs = new Observable({
-  $key:'obs',
+
+log('smutch')
+
+var mysmutch = new Observable({
+  $key:'mysmutch',
+  hurk:true
+})
+
+// mysmutch.$set({
+//   $on: {
+//     $property: function(event, meta) {
+//       log('FIRE PROP', JSON.stringify(meta))
+//     }
+//   }
+// })
+
+mysmutch.$set({
   $on: {
-    $change: function( event, meta ) {
-      //why does getting $key not work?
-      log( 'val change listener (from obs)', this.$path )
-    },
-    $property: function( event, meta ) {
-      log('val property listener (from obs)', JSON.stringify(meta), this.$path)
+    $change: function(event, meta) {
+      log('FIRE change', JSON.stringify(meta))
     }
   }
 })
 
-log('--- now log change and property ------')
 
-obs.$set({
-  x:true
-})
+// log('obs')
 
-log('--- new instance - now log val (from obs) listener and property (from obs context bla)------')
+// var obs = new Observable({
+//   $key:'obs',
+//   $on: {
+//     $change: function( event, meta ) {
+//       //why does getting $key not work?
+//       log( 'val change listener (from obs)', this.$path )
+//     },
+//     $property: function( event, meta ) {
+//       log('val property listener (from obs)', JSON.stringify(meta), this.$path)
+//     }
+//   }
+// })
 
-var bla = new obs.$Constructor({
-  $key:'bla',
-  marcus:'besjes',
-  $on: {
-    $change: {
-      speshField: function( event, meta ) {
-        log('speshField change listener (from bla)', event)
-      }
-    }
-  }
-})
+// log('--- now log change and property ------')
 
-log('---references tests---')
+// obs.$set({
+//   x:true
+// })
 
-var a = new Observable({
-  $key:'a',
-  $val: 10
-})
+// log('--- new instance - now log val (from obs) listener and property (from obs context bla)------')
 
-var b = new Observable({
-  $key:'b',
-  $val: 10,
-  $add: 10
-})
+// var bla = new obs.$Constructor({
+//   $key:'bla',
+//   marcus:'besjes',
+//   $on: {
+//     $change: {
+//       speshField: function( event, meta ) {
+//         log('speshField change listener (from bla)', event)
+//       }
+//     }
+//   }
+// })
 
-console.warn('make c')
+// log('---references tests---')
 
-var c = new Observable({
-  $key:'c',
-  $val: a,
-  $add: b,
-  $on: {
-    $change: function( event, meta ) {
-      // console.log('CHANGE!', this)
-      log('change from c', this.$path, this.$val)
-    },
-    // $new: function( event, meta ) {
-    //   // log( 'new instance!', 'current instance:', this.$path, 'from:', meta.$path )
-    // }
-  }
-})
+// var a = new Observable({
+//   $key:'a',
+//   $val: 10
+// })
 
-log(c.$val) 
+// var b = new Observable({
+//   $key:'b',
+//   $val: 10,
+//   $add: 10
+// })
 
-log('-- update a---- (c should update)')
+// console.warn('make c')
 
-// console.clear()
+// var c = new Observable({
+//   $key:'c',
+//   $val: a,
+//   $add: b,
+//   $on: {
+//     $change: function( event, meta ) {
+//       // console.log('CHANGE!', this)
+//       log('change from c', this.$path, this.$val)
+//     },
+//     // $new: function( event, meta ) {
+//     //   // log( 'new instance!', 'current instance:', this.$path, 'from:', meta.$path )
+//     // }
+//   }
+// })
 
-a.$val = 20
+// log(c.$val) 
 
-// debugger
-// console.clear()
+// log('-- update a---- (c should update)')
 
-log('--d new instance of c expect c to fire (context d) (since set /w key)')
-// //think about this maybe key should not update?
+// // console.clear()
 
-console.warn('make d')
+// a.$val = 20
 
-// //nu word d2x gecalled omdat bound niet word beijgehouden in dingen die geexclude moeten
-// //worden mischien toch in de emitter?
-// //anders is er geen difference...
+// // debugger
+// // console.clear()
 
+// log('--d new instance of c expect c to fire (context d) (since set /w key)')
+// // //think about this maybe key should not update?
 
-//   setup
-//   bind word een array
-//   word altijd meegenomen in stamp
-//   $emit in emitter is responsible voor het uitvoeren van multiple bound things
-//   (dus exec met different bound)
+// console.warn('make d')
 
-//   __bind__ is altijd array ( for now )
-
-//   emitter emit doet exec op multiple --- $emit van obs is wel responsible voor het added?
-
-//   //ook niet called alleen emit nog een keer!
-
-
-
-
-try {
+// // //nu word d2x gecalled omdat bound niet word beijgehouden in dingen die geexclude moeten
+// // //worden mischien toch in de emitter?
+// // //anders is er geen difference...
 
 
-//use uid system for this
-//since d has and update from c and one of its own
-//this will break it
+// //   setup
+// //   bind word een array
+// //   word altijd meegenomen in stamp
+// //   $emit in emitter is responsible voor het uitvoeren van multiple bound things
+// //   (dus exec met different bound)
 
-var d = new c.$Constructor({
-  $key:'d',
-  $on: {
-    $change: {
-      //now this is unique so it will work
-      flups:function(){
-        //moet nog checken of een instance eigen onFn heeft
-        log('flups', this.$path)
-      }
-    }
-  }
-})
+// //   __bind__ is altijd array ( for now )
 
-} catch(e) {
-  console.error(e.stack)
-}
+// //   emitter emit doet exec op multiple --- $emit van obs is wel responsible voor het added?
 
-// console.clear()
-log('update a, expect d and c to fire, fulps as well (context d)')
-a.$val = 30
-
-log('--e new instance of d, fire c context e, flups (context e)')
-
-console.warn('make e')
-
-var e = new d.$Constructor({
-  $key:'e',
-  $add:100
-})
+// //   //ook niet called alleen emit nog een keer!
 
 
-log('--- change a ref fire c context c,d,e ---, flups (context e,d)')
-
-a.$val = 40
 
 
-log('--- change b ref fire c context c,d, NO e ---, flups (context e,d)')
+// try {
 
-b.$val = 40
+
+// //use uid system for this
+// //since d has and update from c and one of its own
+// //this will break it
+
+// var d = new c.$Constructor({
+//   $key:'d',
+//   $on: {
+//     $change: {
+//       //now this is unique so it will work
+//       flups:function(){
+//         //moet nog checken of een instance eigen onFn heeft
+//         log('flups', this.$path)
+//       }
+//     }
+//   }
+// })
+
+// } catch(e) {
+//   console.error(e.stack)
+// }
+
+// // console.clear()
+// log('update a, expect d and c to fire, fulps as well (context d)')
+// a.$val = 30
+
+// log('--e new instance of d, fire c context e, flups (context e)')
+
+// console.warn('make e')
+
+// var e = new d.$Constructor({
+//   $key:'e',
+//   $add:100
+// })
+
+
+// log('--- change a ref fire c context c,d,e ---, flups (context e,d)')
+
+// a.$val = 40
+
+
+// log('--- change b ref fire c context c,d, NO e ---, flups (context e,d)')
+
+// b.$val = 40
 
 
 
