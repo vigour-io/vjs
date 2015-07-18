f23e32c01c39d7cd02cada27c18c38eff37f0e28
Jim de Beer <jim@vigour.io>
2015-07-18 15:37:56 +0200

    added multi instances tests, 'measure' is the object used to measure counts, contexts etc

--- test/change-emitter-instances/index.js
+++ test/change-emitter-instances/index.js
@@ -10,30 +10,52 @@ chai.use(function( _chai, _ ) {
 describe('$change emitter - instances', function() {
 
   var Observable = require('../../lib/observable')
-  var listenerCountersAndContext = {
-    obs:{},
+  var measure = {
+    a:{},
+    instanceOfObs:{}
   }
+  var a
+  var b
 
-  it('create new observable (obs), add $change listener', function() {
+  it( 'create new observable (a), add $change listener', function() {
     
-    listenerCountersAndContext.obs.val = [ 0 ]
+    measure.a.val = [ 0, {} ]
 
-    obs = new Observable({
-      $key:'obs',
+    a = new Observable({
+      $key:'a',
       $on: {
         $change: function( event, meta ) {
-          listenerCountersAndContext.obs.val[0]++
-          listenerCountersAndContext.obs.val[1] = this._$key
+          measure.a.val[0]+=1
+          var keyCnt =  measure.a.val[1][this._$key] 
+          measure.a.val[1][this._$key] = keyCnt ? (keyCnt+1) : 1 
         }
       }
     })
 
-    expect( listenerCountersAndContext.obs.val[0] ).to.equal( 0 )
+    expect( measure.a.val[0] ).to.equal( 0 )
 
-    obs.$val = 'a value'
-    expect( listenerCountersAndContext.obs.val[0] ).to.equal( 1 )
-    expect( listenerCountersAndContext.obs.val[1] ).to.equal( obs._$key )
+    a.$val = 'a value'
+    expect( measure.a.val[0] ).to.equal( 1 )
+    expect( measure.a.val[1].a ).to.equal( 1 )
 
   })
 
+  it(
+    'create new a.$Constructor (b), check if context is correct'
+    , function() {
+    b = new a.$Constructor({
+      $key:'b'
+    })  
+    expect( measure.a.val[0] ).to.equal( 2 )
+    expect( measure.a.val[1].b ).to.equal( 1 )
+  })
+
+  it( 'change a.$val', function() {
+    a.$val = 'a change'
+    expect( measure.a.val[0] ).to.equal( 3 )
+    expect( measure.a.val[1].a ).msg('a context').to.equal( 2 )
+    expect( measure.a.val[1].b ).msg('b context').to.equal( 2 )
+  })
+
 })
+
