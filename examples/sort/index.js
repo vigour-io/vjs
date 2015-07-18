b602201b8f0d3a638f7af8fda723d28e4ff2b061
marcusbesjes <marcusbesjes@gmail.com>
2015-07-18 02:12:35 +0200

    operator chaining

--- examples/sort/index.js
+++ examples/sort/index.js
@@ -42,7 +42,42 @@ console.log('-------------- $val:')
 $val.each(function(value, key){
   console.log('>', key, value.$origin.$val)
 })
+console.clear()
+console.log('\n\n=============================== sort by self with add')
 
+var items = ['A', 'a', 'za', 'fr23', 'ZA', 'a']
+items.$sort = true
+items.$add = ['11111111']
+
+var obs = new Observable(items)
+obs._$key = 'obs'
+
+var $val = obs.$val
+
+console.log('-------------- obs:')
+obs.each(function(value, key){
+  console.log('>', key, value.$origin.$val)
+})
+
+console.log('-------------- $val:')
+$val.each(function(value, key){
+  console.log('>', key, value.$origin.$val)
+})
+
+console.log('++++++++++++++ change some:')
+obs[0].$val = 'GOAN CHANGED'
+
+console.log('-------------- obs.$sort._result:')
+obs.$sort._results.each(function(value, key){
+  console.log('>', key, value.$origin.$val)
+})
+
+console.log('-------------- $val:')
+$val.each(function(value, key){
+  console.log('>', key, value.$origin.$val)
+})
+
+throw new Error('wait')
 
 
 console.log('\n\n=============================== sort by key')
