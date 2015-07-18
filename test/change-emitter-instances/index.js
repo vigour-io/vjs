4103d91cbd65c499e87b9551cede83e40362d7a6
Jim de Beer <jim@vigour.io>
2015-07-18 16:03:44 +0200

    first instance update tests (failing)

--- test/change-emitter-instances/index.js
+++ test/change-emitter-instances/index.js
@@ -17,8 +17,7 @@ describe('$change emitter - instances', function() {
   var a
   var b
 
-  it( 'create new observable (a), add $change listener', function() {
-    
+  it( 'create new observable (a), add $change listener', function() {    
     measure.a.val = [ 0, {} ]
 
     a = new Observable({
@@ -28,33 +27,37 @@ describe('$change emitter - instances', function() {
           measure.a.val[0]+=1
           var keyCnt =  measure.a.val[1][this._$key] 
           measure.a.val[1][this._$key] = keyCnt ? (keyCnt+1) : 1 
+          console.log('ghello?', this.$path)
         }
       }
     })
-
     expect( measure.a.val[0] ).to.equal( 0 )
 
     a.$val = 'a value'
     expect( measure.a.val[0] ).to.equal( 1 )
     expect( measure.a.val[1].a ).to.equal( 1 )
-
   })
 
-  it(
-    'create new a.$Constructor (b), check if context is correct'
-    , function() {
+  it( 'create new a.$Constructor (b), check if context is correct', function() {
     b = new a.$Constructor({
       $key:'b'
+      //does work when adding your own emitter...
     })  
+
+    expect( a.$on._instances.length )
+      .msg('a.$on._instances has correct length').to.equal(1)
+    expect( a.$on._instances[0] )
+      .msg('b is a.$on._instances[0]').to.equal(b)
+
     expect( measure.a.val[0] ).to.equal( 2 )
     expect( measure.a.val[1].b ).to.equal( 1 )
   })
 
   it( 'change a.$val', function() {
     a.$val = 'a change'
-    expect( measure.a.val[0] ).to.equal( 3 )
     expect( measure.a.val[1].a ).msg('a context').to.equal( 2 )
     expect( measure.a.val[1].b ).msg('b context').to.equal( 2 )
+    expect( measure.a.val[0] ).to.equal( 4 )
   })
 
 })
