94c18c39ed84d32df26f3b279f3ecc02add2dd3c
Jim de Beer <jim@vigour.io>
2015-07-18 14:45:02 +0200

    added change emitter - no instances, passon tests, tests for listens stores

--- test/change-emitter-no-instances/index.js
+++ test/change-emitter-no-instances/index.js
@@ -12,12 +12,15 @@ describe('$change emitter - no instances', function() {
   var Observable = require('../../lib/observable')
   var listenerCounters = {
     obs:{},
-    obs2:{}
+    obs2:{},
+    obs3:{}
   }
 
   var obs
   var obs2
+  var obs3
   var referencedObs
+  var referencedObs2
 
   it('create new observable (obs), add $change listener', function() {
     
@@ -113,6 +116,16 @@ describe('$change emitter - no instances', function() {
 
     expect( listenerCounters.obs2.val ).msg( 'val listener' ).to.equal( 0 )
 
+    expect( obs2 ).to.have.property( '$listensOnBase' )
+
+    var keyCount = 0
+    obs2.$listensOnBase.each(function(property, key) {
+      keyCount++
+    })
+    expect( keyCount ).msg('amount of listeners on listensOnBase').to.equal( 1 )
+    
+    expect( obs2.$listensOnBase ).to.have.property( 1 )
+
     referencedObs.$val = 'changed a string'
 
     expect( listenerCounters.obs2.val ).msg( 'val listener' ).to.equal( 1 )
@@ -129,6 +142,48 @@ describe('$change emitter - no instances', function() {
     referencedObs2.$val = 'changed a string'
 
     expect( listenerCounters.obs2.val ).msg( 'val listener' ).to.equal( 3 )
+
+  })
+
+  it('passon tests on obs3', function() {
+    
+    listenerCounters.obs3.val = 0
+
+    obs3 = new Observable({
+      $key: 'obs3',
+      $on: {
+        $change: {
+          val: [ 
+            function( event, meta, base, extraArg1, extraArg2 ) {
+              listenerCounters.obs3.val++
+              expect( extraArg1 ).to.equal( 'extra1' )
+              expect( extraArg2 ).to.equal( 'extra2' )
+            },
+            referencedObs,
+            'extra1',
+            'extra2'
+          ]
+        }
+      }
+    })
+
+    referencedObs.$val = 'lets test passon'
+    expect( listenerCounters.obs3.val ).to.equal( 0 )
+
+    expect( referencedObs ).to.have.property( '$listensOnPasson' )
+
+    var keyCount = 0
+    referencedObs.$listensOnPasson.each(function(property, key) {
+      keyCount++
+    })
+    expect( keyCount ).msg('amount of listeners on listensOnPasson').to.equal( 1 )
+    expect( referencedObs.$listensOnPasson ).to.have.property( 1 )
+
+    obs3.$val = referencedObs
+    expect( listenerCounters.obs3.val ).to.equal( 1 )
+    
+    referencedObs.$val = 'lets test passon, now it should fire'
+    expect( listenerCounters.obs3.val ).to.equal( 2 )
 
   })
 
