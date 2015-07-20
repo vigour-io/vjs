f97b8bb559fb2f863cb777dcd2a6aaf59b63f2da
Jim de Beer <jim@vigour.io>
2015-07-20 18:50:08 +0200

    added basic remove on base, added tests #3

--- test/remove/index.js
+++ test/remove/index.js
@@ -1,17 +1,35 @@
-describe('$change emitter - instances - references', function() {
+describe('remove', function() {
 
   var Observable = require('../../lib/observable')
+  var util = require('../../lib/util')
   var measure = {}
   var a
 
-  it( 'create new observable --> a', function() {    
-    //making new instances manges reference updates
-     a = new Observable({
+  it( 'create new observable --> a and remove using method', function() {    
+    a = new Observable({
       $key:'a',
       $val:1
-     })
+    })
+    a.remove()
+    expect(a).to.be.empty
+  })
 
-     
+  it( 'create new observable --> a and remove using a set with null', function() {    
+    a = new Observable({
+      $key:'a',
+      $val:1
+    }) 
+    a.$val = null  
+    expect(a).to.be.empty
+  })
+
+  it( 'create new observable --> a and remove field blarf', function() {    
+    a = new Observable({
+      $key:'a',
+      $blarf:true
+    }) 
+    a.$blarf.$val = null  
+    expect(a.$blarf).to.be.an('undefined');
   })
 
 })
