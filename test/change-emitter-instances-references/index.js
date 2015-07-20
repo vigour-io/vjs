c3c651dda1e02172466c25973bc3881b649a2b1e
Jim de Beer <jim@vigour.io>
2015-07-20 15:16:38 +0200

    start of resolving #42, added more tests for references

--- test/change-emitter-instances-references/index.js
+++ test/change-emitter-instances-references/index.js
@@ -5,14 +5,16 @@ describe('$change emitter - instances - references', function() {
     a:{},
     b:{},
     c:{},
-    aRef:{}
+    aRef:{},
+    bRef:{}
   }
   var aRef
+  var bRef
   var a
   var b
   var c
 
-  it( 'create new observable --> aRef, add change listener "val"', function() {    
+  it( 'create new observable --> aRef', function() {    
     //making new instances manges reference updates
      aRef = new Observable({
       $key:'aRef',
@@ -20,13 +22,19 @@ describe('$change emitter - instances - references', function() {
      })
   })
 
-  it( 'create new observable --> a, add change listener "val"', function() {    
-    measure.a.val = { total: 0 }
+  it( 'create new observable --> a, add change listener "val", set val to aRef', function() {    
+    measure.a.val = { total: 0, origin:{} }
 
     a = new Observable({
       $key:'a',
       $on: {
-        $change: function( event, meta ) {
+        $change: function( event ) {
+
+          console.log('a', this.$path, event.$origin.$path)
+
+          var originkeyCnt = measure.a.val.origin[event.$origin._$key]
+          measure.a.val.origin[event.$origin._$key] = originkeyCnt ? (originkeyCnt+1) : 1 
+
           var keyCnt =  measure.a.val[this._$key] 
           measure.a.val.total+=1
           measure.a.val[this._$key] = keyCnt ? (keyCnt+1) : 1 
@@ -53,6 +61,99 @@ describe('$change emitter - instances - references', function() {
     expect( measure.a.val.a ).msg('a context').to.equal( 2 )
     expect( measure.a.val.b ).msg('b context').to.equal( 2 )
     expect( measure.a.val.total ).to.equal( 4 )
+
+    expect( measure.a.val.origin.a ).msg('a origin').to.equal( 1 )
+    expect( measure.a.val.origin.b ).msg('b origin').to.equal( 1 )
+    expect( measure.a.val.origin.aRef ).msg('aRef origin').to.equal( 2 )
+
+  })
+
+  it( 'add change listener "second" on b', function() {
+    measure.b.second = { total: 0, origin:{} }
+    b.$val = {
+      $on: {
+        $change: {
+          second: function( event ) {
+            var originkeyCnt = measure.b.second.origin[event.$origin._$key]
+            measure.b.second.origin[event.$origin._$key] = originkeyCnt ? (originkeyCnt+1) : 1 
+
+            var keyCnt =  measure.b.second[this._$key] 
+            measure.b.second.total+=1
+            measure.b.second[this._$key] = keyCnt ? (keyCnt+1) : 1 
+          }
+        }
+      }
+    } 
+
+    expect( measure.a.val.a ).msg('a context').to.equal( 2 )
+    expect( measure.a.val.b ).msg('b context').to.equal( 3 )
+    expect( measure.a.val.total ).to.equal( 5 )
+
+    expect( measure.b.second.total ).to.equal( 0 )
+
+    expect( measure.a.val.origin.aRef ).msg('aRef origin').to.equal( 2 )
+
+  })
+
+  it( 'add property "prop1" on a', function() {
+    a.$val = {
+      prop1:true
+    }
+   
+    expect( measure.a.val.a ).msg('a context').to.equal( 3 )
+    expect( measure.a.val.b ).msg('b context').to.equal( 4 )
+    expect( measure.a.val.total ).to.equal( 7 )
+
+    expect( measure.b.second.b ).msg('b context').to.equal( 1 )
+    expect( measure.b.second.total ).to.equal( 1 )
+  })
+
+  it( 'change aRef', function() {
+    aRef.$val = 'another change'
+    
+    expect( measure.a.val.a ).msg('a context (a val)').to.equal( 4 )
+    expect( measure.a.val.b ).msg('b context (a val)').to.equal( 5 )
+    expect( measure.a.val.total ).to.equal( 9 )
+
+    expect( measure.b.second.b ).msg('b context (b second)').to.equal( 2 )
+    expect( measure.b.second.total ).to.equal( 2 )
+
+    expect( measure.a.val.origin.aRef ).msg('aRef origin (a val)').to.equal( 4 )
+    expect( measure.b.second.origin.aRef ).msg('aRef origin )(b context)').to.equal( 1 )
+  })
+
+  it( 'create new aRef --> bRef', function() {
+
+    console.clear()
+
+    bRef = new aRef.$Constructor({
+      $key:'bRef'
+    }) 
+
+    aRef.$val = 'wow more change'
+
+    console.log(measure.a.val.origin)
+
+    expect( measure.a.val.a ).msg('a context (a val)').to.equal( 5 )
+    expect( measure.a.val.b ).msg('b context (a val)').to.equal( 6 )
+    expect( measure.a.val.total ).to.equal( 11 )
+
+    expect( measure.b.second.b ).msg('b context (b second)').to.equal( 3 )
+    expect( measure.b.second.total ).to.equal( 3 )
+
+  })
+
+  it( 'change bRef', function() {
+
+    bRef.$val = 'im changeing bRef'
+
+    expect( measure.a.val.a ).msg('a context (a val)').to.equal( 5 )
+    expect( measure.a.val.b ).msg('b context (a val)').to.equal( 6 )
+    expect( measure.a.val.total ).to.equal( 11 )
+
+    expect( measure.b.second.b ).msg('b context (b second)').to.equal( 3 )
+    expect( measure.b.second.total ).to.equal( 3 )
+
   })
 
 })
