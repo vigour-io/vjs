1b6a2eed85fc0b36c99f89a13241f57e4eb94cda
Jim de Beer <jim@vigour.io>
2015-07-23 22:03:20 +0200

    added reference and property tests ( no instances )
      - fixed resetting references (removeslistener) closes #54

--- test/all/index.js
+++ test/all/index.js
@@ -1,6 +1,6 @@
+require('../emitter-combined')
 require('../change-emitter-instances')
 require('../change-emitter-instances-references')
 require('../change-emitter-no-instances')
 require('../change-emitter-instances-listener-removal')
 require('../remove')
-require('../emitter-combined')
--- test/emitter-combined/index.js
+++ test/emitter-combined/index.js
@@ -12,23 +12,62 @@ describe('emitter - combined', function() {
   var aRef
 
 
-  it( 'create new observable --> a', function() {    
-     aRef = new Observable({
+  it( 'create new observable --> a --> use references change a', function() {    
+
+    measure.a.$reference = {
+      val: { total: 0 }
+    }   
+
+    measure.a.$change = {
+      val: { total: 0 }
+    }
+
+    measure.a.$property = {
+      val: { total: 0 }
+    }
+
+    aRef = new Observable({
       $key: 'aRef',
       $val: 'a value for aRef'
-     })
+    })
 
-     a = new Observable({
+    a = new Observable({
       $key: 'a',
       $on: {
-        $change:function() {
-          console.log('lemme fire!')
+        $change:function( event, meta ) {
+           measure.a.$change.val.total++
+        },
+        $reference: function( event, meta ) {
+          measure.a.$reference.val.total++
+        },
+        $property:function( event, meta ) {
+          measure.a.$property.val.total++
         }
       },
       $val: aRef
-     })
+    })
 
-     a.$val = 10
+    expect( aRef.$on.$change.$base[1] ).to.equal( a )
+
+    a.$val = 10
+    expect( aRef.$on.$change.$base ).to.be.null
+    expect( measure.a.$reference.val.total ).to.equal( 1 )
+    expect( measure.a.$change.val.total ).to.equal( 1 )
+
+    a.$val = 20
+    expect( measure.a.$reference.val.total ).to.equal( 1 )
+    expect( measure.a.$change.val.total ).to.equal( 2 )
+    expect( measure.a.$property.val.total ).to.equal( 0 )
+
+    a.$set({
+      $val: aRef,
+      prop1:true
+    })
+
+    expect( measure.a.$reference.val.total ).to.equal( 2 )
+    expect( measure.a.$change.val.total ).to.equal( 3 )
+    expect( measure.a.$property.val.total ).to.equal( 1 )
+    expect( aRef.$on.$change.$base[2] ).to.equal( a )
 
   })
 
