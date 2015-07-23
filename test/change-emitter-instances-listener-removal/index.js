886646b74ea8fbb33cdfc65f8a4b1ba34171ee7e
Jim de Beer <jim@vigour.io>
2015-07-23 16:19:39 +0200

    added base type tests and removal for removeAndFind #45

--- test/change-emitter-instances-listener-removal/index.js
+++ test/change-emitter-instances-listener-removal/index.js
@@ -111,6 +111,24 @@ describe('$change emitter - instances - listener - removal', function() {
     expect( a.$on.$change.$passon ).to.be.ok
     expect( b.$on.$change.$passon ).to.be.null
 
+    ref.$set({
+      $on: {
+        $change: [ function(){} , a ]
+      }
+    })
+
+    expect( ref.$on.$change.$base ).to.be.ok
+    expect( ref.$on.$change.$passon ).to.be.ok
+    expect( util.isEmpty( a.$listensOnBase ) ).to.be.false
+    expect( util.isEmpty( a.$listensOnPasson ) ).to.be.false
+
+    ref.removeListener( '$change', a )
+
+    expect( ref.$on.$change.$base ).to.be.null
+    expect( ref.$on.$change.$passon ).to.be.null
+    expect( util.isEmpty( a.$listensOnBase ) ).to.be.true
+    expect( util.isEmpty( a.$listensOnPasson ) ).to.be.true
+
   })
 
 })
