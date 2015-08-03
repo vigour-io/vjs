d963a43e78a189cd9b9dba1e0ebc333d682e8ae2
nikoloza <nika.tomadze@gmail.com>
2015-08-03 18:30:05 +0200

    comment tests

--- test/utils/index.js
+++ test/utils/index.js
@@ -6,8 +6,11 @@ describe('utils', function() {
   describe('isNumber', function() {
     it('should check if parameter is a number and return a boolean value', function() {
 
+      // must pass
       expect(utils.isNumber(2)).to.be.ok
       expect(utils.isNumber(-10)).to.be.ok
+
+      // must fail
       expect(utils.isNumber('2')).to.not.be.ok
       expect(utils.isNumber('two')).to.not.be.ok
 
@@ -17,9 +20,12 @@ describe('utils', function() {
   describe('isLikeNumber', function() {
     it('should check if parameter is like a number ("2", "-10") and return a boolean value', function() {
 
+      // must pass
       expect(utils.isLikeNumber(2)).to.be.ok
       expect(utils.isLikeNumber(-10)).to.be.ok
       expect(utils.isLikeNumber('2')).to.be.ok
+
+      // must fail
       expect(utils.isLikeNumber('two')).to.not.be.ok
 
     })
@@ -28,10 +34,13 @@ describe('utils', function() {
   describe('isPlainObj', function() {
     it('should check if parameter is a plain object and return a boolean value', function() {
 
+      // must pass
       expect(utils.isPlainObj({})).to.be.true
       expect(utils.isPlainObj({x: 1})).to.be.true
-      expect(utils.isPlainObj('two')).to.not.be.true
       expect(utils.isPlainObj([])).to.be.true
+
+      // mast fail
+      expect(utils.isPlainObj('two')).to.not.be.true
 
     })
   })
@@ -43,27 +52,31 @@ describe('utils', function() {
         return arguments
       }
 
-      expect(
-        Array.isArray(
-          testFunc(1,2,3,4)
-        )
-      ).to.not.be.true
-
+      // must pass
       expect(
         Array.isArray(
           utils.convertToArray(testFunc(1,2,3,4))
         )
       ).to.be.true
+
+      // must fail
+      expect(
+        Array.isArray(
+          testFunc(1,2,3,4)
+        )
+      ).to.not.be.true
     })
   })
 
   describe('isEmpty', function () {
     it('should check if object is empty and return a boolean value', function () {
 
+      // must pass
       expect(
         utils.isEmpty({})
       ).to.be.true
 
+      // must fail
       expect(
         utils.isEmpty({a: 1})
       ).to.not.be.true
@@ -72,9 +85,9 @@ describe('utils', function() {
   })
 
   describe('isRemoved', function () {
-    it('should check if ...', function () {
+    it('should ...', function () {
 
-      // test
+      // ...
 
     })
   })
