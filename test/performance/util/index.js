0d307bbfc0fa1b758806adc2083bd7d882fce2fe
Jim de Beer <jim@vigour.io>
2015-08-03 16:49:39 +0200

    added isLikeNumber util
    - its fast! (allmost always faster or as fast a lodash/lang/isNumber
    - test is made in perf/util

--- test/performance/util/index.js
+++ test/performance/util/index.js
@@ -5,11 +5,39 @@ describe( 'util test', function() {
   var Base = require('../../../lib/base')
   var util = require('../../../lib/util')
 
+  it('isLikeNumber vs lodash isNumber', function( done ) {
+    this.timeout(5000)
+    var convertToArray = util.convertToArray
+    var slice = Array.prototype.slice
+    var amount = 1e6
+    var isLikeNumber = util.isLikeNumber
+    var isNumber = require('lodash/lang/isNumber')
+
+    expect( function() {
+      for(var i = 0; i < amount; i++ ) {
+        isLikeNumber( 'a'+i )
+      }
+      for(var i = 0; i < amount; i++ ) {
+        isLikeNumber( i )
+      }
+    }).performance({
+      margin:3,
+      method:function() {
+        for(var i = 0; i < amount; i++ ) {
+          isNumber( 'a'+i )
+        }
+        for(var i = 0; i < amount; i++ ) {
+          isNumber( i )
+        }
+      }
+    }, done )
+  })
+
   it('convertToArray vs Array.prototype.slice', function( done ) {
     this.timeout(5000)
     var convertToArray = util.convertToArray
     var slice = Array.prototype.slice
-    var amount = 1000e3
+    var amount = 1e6
 
     expect( function() {
       function fn() {
