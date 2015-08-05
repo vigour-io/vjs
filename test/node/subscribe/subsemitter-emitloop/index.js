973f2bdd8718d957865de390a6ea45b9eb997949
marcusbesjes <marcusbesjes@gmail.com>
2015-08-05 11:03:51 +0200

    start on subscribe tests

--- test/node/subscribe/subsemitter-emitloop/index.js
+++ test/node/subscribe/subsemitter-emitloop/index.js
@@ -1,6 +1,53 @@
 describe('subsemitter-emitloop', function() {
 
-	it.skip('emits on SubsEmitter are metaPostponed ', function() {
+	var Event
+	var Emitter
+	var SubsEmitter
+	var Observable
+
+	it.should('require', function(){
+		Event = require( '../lib/event' )
+		Emitter = require( '../lib/emitter' )
+		SubsEmitter = require( '../lib/observable/subscribe/emitter' )
+		Observable = require( '../lib/observable' )
+	})
+
+	it.skip('emits on SubsEmitter are meta-postponed ', function() {
+
+		var obs = new Observable()
+
+		var subsemitter = new SubsEmitter({
+			listener: function( ) {
+				console.log('subscription handler fired!')
+			}
+		})
+
+		var emitterA = new Emitter({
+			$change: {
+				$val: function(event) {
+					console.log('emitterA is fired')
+				}
+			}
+		})
+
+		var emitterB = new Emitter({
+			$change: {
+				$val: function(event) {
+					console.log('emitterB is fired')
+					subsemitter.$emit(event, obs)
+					emitterA.$emit(event, obs)
+					console.log('check out dat event!', event.$postponed)
+				}
+			}
+		})
+
+		var e = new Event()
+
+		emitterB.$emit(e, obs)
+		
+
+
+
 
 	})
 	
