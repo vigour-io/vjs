504d271f6c4ef1e361273ba2e5a4ec87887dde69
Jim de Beer <jim@vigour.io>
2015-08-07 13:38:08 +0200

    testing tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
-// var gaston = require('gaston')
-console.log( '?' )
+var gaston = require('gaston')
+// console.log( '?' )
+// gaston.
+
+// gaston.test.
+
 //get from config- pckg!
