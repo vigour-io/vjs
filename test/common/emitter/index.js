4c9dca0d4c045fb89dd0111366fd03349026f83b
Jim de Beer <jim@vigour.io>
2015-08-21 11:28:49 +0200

    added removeListener test

--- test/common/emitter/index.js
+++ test/common/emitter/index.js
@@ -1,10 +1,10 @@
 var Emitter = require('../../../lib/emitter')
 var Event = require('../../../lib/event')
 
-describe('emitter', function() {
+describe( 'emitter', function() {
   console.clear()
 
-  describe('add listener and fire once', function() {
+  describe( 'add listener and fire once', function() {
     var a = new Emitter()
     var cnt = 0
     a.on(function( event, type ){
@@ -13,6 +13,16 @@ describe('emitter', function() {
     a.emit()
     it('should have fired once',function() {
       expect( cnt ).to.equal(1)
+    })
+  })
+
+  describe( 'add and remove listener', function() {
+    var a = new Emitter()
+    function listener( event, type ) {}
+    a.on( listener )
+    a.off( listener )
+    it('should not have $fn field',function() {
+      expect( a.$fn ).to.be.not.ok
     })
   })
 
