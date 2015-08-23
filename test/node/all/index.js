d54ffad956d221a160484ec7c2850083be5f7375
Jim de Beer <jim@vigour.io>
2015-08-23 18:10:34 +0200

    removed node folder

--- test/node/all/index.js
+++
@@ -1,8 +0,0 @@
-require('../base')
-require('../methods/')
-//add some more set checks and tests
-//nested case etc
-require('../event' )
-require( '../emitter' )
-require( '../observable' )
-require( '../utils' )
