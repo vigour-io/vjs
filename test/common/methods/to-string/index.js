06bade87c09b714b7d2b75902d16ffe25a8f8b09
marcusbesjes <marcusbesjes@gmail.com>
2015-09-03 15:17:51 +0200

    make toString nice for logging

--- test/common/methods/to-string/index.js
+++ test/common/methods/to-string/index.js
@@ -16,8 +16,14 @@ describe('toString', function() {
   })
 
   it('should convert and return string representation of the object', function() {
-    expect(a.toString()).to.eql('{\n  "x": {\n    "y": {\n      "$val": 123\n    }\n  }\n}')
+    expect(a.toString()).to.eql('{\n  "x": {\n    "y": 123\n  }\n}')
   })
+
+  it('should include $val fields if second argument is true', function() {
+    expect(a.toString(false, true)).to.eql('{\n  "x": {\n    "y": {\n      "$val": 123\n    }\n  }\n}')
+  })
+
+
 
   it('should exclude some properties when passed custom exclude', function() {
     var result = a.toString(function(key) {
