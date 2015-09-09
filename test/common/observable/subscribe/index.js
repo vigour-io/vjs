2bb178125e0a54e4492cbc199221a64faad9945d
Jim de Beer <jim@vigour.io>
2015-09-09 16:24:20 +0200

    start subscribe

--- test/common/observable/subscribe/index.js
+++ test/common/observable/subscribe/index.js
@@ -1,52 +1,25 @@
-var Observable = require('../../../../../lib/observable')
+var Observable = require('../../../../lib/observable')
 
-describe('subscribe on existing field',function(){
+console.clear()
+
+describe('subscribe on field', function() {
 
 	var a = new Observable({
 		aField:1
 	})
 
 	a.subscribe({
-		aField:true	
+		aField:true
 	},function(){
 		console.log('go!')
 	},'special')
+
+})
+
+
 
 	// a.subscribe({
 	// 	"*":true //any
 	// 	"&":true //deep
 	// 	somefield:true //field
 	// },'aField')
-
-})
-
-describe('subscribe on non-existing field',function(){
-
-	var a = new Observable()
-	a.subscribe({
-		aField:true	
-	})
-
-})
-
-describe('subscribe on existing ref field',function(){
-
-	var a = new Observable({
-		aField:1
-	})
-	var b = new Observable(a)
-	b.subscribe({
-		aField:true	
-	})
-
-})
-
-describe('subscribe on non-existing ref field',function(){
-
-	var a = new Observable()
-	var b = new Observable(a)
-	b.subscribe({
-		aField:true	
-	})
-
-})
