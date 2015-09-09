b7479692f7c79889f8657edf750db009be1040c1
Youri Daamen <me@youz.io>
2015-09-09 20:33:58 +0200

    subscribe references

--- test/common/observable/subscribe/index.js
+++ test/common/observable/subscribe/index.js
@@ -1,6 +1,5 @@
 var Observable = require('../../../../lib/observable')
 var count
-console.clear()
 
 beforeEach(function(){
 	count = 0
@@ -125,7 +124,237 @@ describe('subscribe on non-existent nested field in non-existent field', functio
 
 })
 
+describe('subscribe on any existing field', function() {
 
+	it( 'should fire once', function() {
+		var a = new Observable({
+			aField:1
+		})
+
+		a.subscribe({
+			"*":true
+		},function(){
+			count++
+		})
+
+		a.aField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on any non-existing field', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable()
+
+		a.subscribe({
+			"*":true
+		},function(){
+			count++
+		})
+
+		a.set({aField:1})
+
+		a.aField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on existing parent', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable()
+
+		var b = new Observable({
+			$key:'b',
+			a:{ $useVal: a }
+		})
+
+		a.subscribe({
+			$parent:true
+		},function(){
+			count++
+		})
+
+		b.$val = 1
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on non-existing parent', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable()
+
+		a.subscribe({
+			$parent:true
+		},function(){
+			count++
+		})
+
+		var b = new Observable({
+			$key:'b',
+			a:{ $useVal: a }
+		})
+
+		b.$val = 1
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on existing field on non-existing parent', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable()
+
+		a.subscribe({
+			$parent:{
+				bField:true
+			},
+		},function(){
+			count++
+		})
+
+		var b = new Observable({
+			$key:'b',
+			bField:1,
+			a:{ $useVal: a }
+		})
+
+		b.bField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on non-existing field on non-existing parent', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable()
+
+		a.subscribe({
+			$parent:{
+				bField:true
+			},
+		},function(){
+			count++
+		})
+
+		var b = new Observable({
+			$key:'b',
+			a:{ $useVal: a }
+		})
+
+		b.set({
+			bField:1
+		})
+
+		b.bField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on referenced obj, 1 level, existing field', function() {
+
+	console.clear()
+
+	it( 'should fire once', function() {
+		
+		var a = new Observable({
+			aField:1
+		})
+
+		var b = new Observable(a)
+
+		b.subscribe({
+			aField:true
+		},function(){
+			count++
+		})
+
+		a.aField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on referenced obj, 2 levels, existing field', function() {
+
+	console.clear()
+
+	it( 'should fire once', function() {
+		
+		var a = new Observable({
+			aField:1
+		})
+
+		var b = new Observable(a)
+		var c = new Observable(b)
+
+		c.subscribe({
+			aField:true
+		},function(){
+			count++
+		})
+
+		a.aField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on referenced obj, 2 levels, several locations', function() {
+
+	console.clear()
+
+	it( 'should fire once', function() {
+		
+		var a = new Observable({
+			aField:1
+		})
+
+		var b = new Observable({
+			$val:a,
+			bField:1
+		})
+		var c = new Observable(b)
+
+		c.subscribe({
+			aField:true,
+			bField:true
+		},function(){
+			count++
+		})
+
+		a.aField.$val = 2
+		b.bField.$val = 2
+
+		expect(count).equals(2)
+
+	})
+
+})
 
 //op subsEmitter komt fire waar je een listener aan kan meegeven en een target
 
