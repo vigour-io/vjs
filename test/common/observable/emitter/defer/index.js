998e66a39c7b78d197fcf65e5ef9b31d4d7a09c0
Jim de Beer <jim@vigour.io>
2015-09-09 09:14:25 +0200

    cancel in defer, clearing inProgress

--- test/common/observable/emitter/defer/index.js
+++ test/common/observable/emitter/defer/index.js
@@ -11,59 +11,87 @@ describe('defer', function() {
   var Observable = require('../../../../../lib/observable')
 
   it( 'fire with a timeout of 200ms', function(done) {
-
     var a = new Observable({
-      $key:'a', //this will just set $key
       $on: {
         $change: {
           $val: function() {
+            expect(this.$on.$change.$defer.$inProgress).to.be.null
             done()
           },
-          // defer can be an object or function
-          $defer: {
-            $val: function( emit ) {
-              setTimeout( emit, 200 )
-            }
+          $defer: function( emit, event, defer ) {
+            setTimeout( emit, 200 )
           }
         }
       }
     })
-
     a.$val = 'hello'
-
   })
 
   it( 'remove defer', function( done ) {
     var cnt = 0
     var a = new Observable({
-      $key:'a', //this will just set $key
       $on: {
         $change: {
           $val: function() {
             cnt++
           },
-          // defer can be an object or function
           $defer: {
             $val: function( emit, event, defer ) {
-              defer._$timeout = setTimeout( emit, 200 )
+              this._$timeout = setTimeout( emit, 200 )
             },
-            $cancel: function( event, defer ) { //add this a default flag on base?
-              clearTimeout( defer._$timeout )
+            $cancel: function( event, defer ) {
+              clearTimeout( this._$timeout )
             }
           }
         }
       }
     })
-
     a.$val = 'hello'
-
     a.remove()
-
     setTimeout(function() {
       expect(cnt).to.equal(0)
       done()
-    }, 400)
+    }, 300)
+  })
 
+  it( 'clear when returning', function() {
+    var cnt = 0
+    var a = new Observable({
+      $on: {
+        $change: {
+          $val: function() {
+            cnt++
+          },
+          $defer: function( emit, event, defer ) {
+            return true
+          }
+        }
+      }
+    })
+    a.$val = 'hello'
+    expect(a.$on.$change.$defer.$inProgress).to.be.null
+    expect(cnt).to.equal(0)
+  })
+
+  it( 'clear inProgress on cancel', function() {
+    var cnt = 0
+    var a = new Observable({
+      $on: {
+        $change: {
+          $val: function() {
+            cnt++
+          },
+          $defer: {
+            $val: function( emit, event, defer ) {
+              defer.$cancel()
+            }
+          }
+        }
+      }
+    })
+    a.$val = 'hello'
+    expect(a.$on.$change.$defer.$inProgress).to.be.null
+    expect(cnt).to.equal(0)
   })
 
 })
