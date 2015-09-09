0b2eb1b5398891b5f1895e4dddfaad54f291d493
Jim de Beer <jim@vigour.io>
2015-09-09 09:42:03 +0200

    added extra comments, moved includes to include util -- renamed to isIncluded

--- test/common/observable/emitter/defer/index.js
+++ test/common/observable/emitter/defer/index.js
@@ -10,88 +10,94 @@ describe('defer', function() {
 
   var Observable = require('../../../../../lib/observable')
 
-  it( 'fire with a timeout of 200ms', function(done) {
-    var a = new Observable({
-      $on: {
-        $change: {
-          $val: function() {
-            expect(this.$on.$change.$defer.$inProgress).to.be.null
-            done()
-          },
-          $defer: function( emit, event, defer ) {
-            setTimeout( emit, 200 )
-          }
-        }
-      }
-    })
-    a.$val = 'hello'
-  })
+  describe( 'single instance', function() {
 
-  it( 'remove defer', function( done ) {
-    var cnt = 0
-    var a = new Observable({
-      $on: {
-        $change: {
-          $val: function() {
-            cnt++
-          },
-          $defer: {
-            $val: function( emit, event, defer ) {
-              this._$timeout = setTimeout( emit, 200 )
+    it( 'fire with a timeout of 200ms', function(done) {
+      var a = new Observable({
+        $on: {
+          $change: {
+            $val: function() {
+              expect(this.$on.$change.$defer.$inProgress).to.be.null
+              done()
             },
-            $cancel: function( event, defer ) {
-              clearTimeout( this._$timeout )
+            $defer: function( emit, event, defer ) {
+              setTimeout( emit, 200 )
             }
           }
         }
-      }
+      })
+      a.$val = 'hello'
     })
-    a.$val = 'hello'
-    a.remove()
-    setTimeout(function() {
+
+    it( 'remove defer', function( done ) {
+      var cnt = 0
+      var a = new Observable({
+        $on: {
+          $change: {
+            $val: function() {
+              cnt++
+            },
+            $defer: {
+              $val: function( emit, event, defer ) {
+                this._$timeout = setTimeout( emit, 200 )
+              },
+              $cancel: function( event, defer ) {
+                clearTimeout( this._$timeout )
+              }
+            }
+          }
+        }
+      })
+      a.$val = 'hello'
+      a.remove()
+      setTimeout(function() {
+        expect(cnt).to.equal(0)
+        done()
+      }, 300)
+    })
+
+    it( 'clear when returning', function() {
+      var cnt = 0
+      var a = new Observable({
+        $on: {
+          $change: {
+            $val: function() {
+              cnt++
+            },
+            $defer: function( emit, event, defer ) {
+              return true
+            }
+          }
+        }
+      })
+      a.$val = 'hello'
+      expect(a.$on.$change.$defer.$inProgress).to.be.null
       expect(cnt).to.equal(0)
-      done()
-    }, 300)
-  })
-
-  it( 'clear when returning', function() {
-    var cnt = 0
-    var a = new Observable({
-      $on: {
-        $change: {
-          $val: function() {
-            cnt++
-          },
-          $defer: function( emit, event, defer ) {
-            return true
-          }
-        }
-      }
     })
-    a.$val = 'hello'
-    expect(a.$on.$change.$defer.$inProgress).to.be.null
-    expect(cnt).to.equal(0)
-  })
 
-  it( 'clear inProgress on cancel', function() {
-    var cnt = 0
-    var a = new Observable({
-      $on: {
-        $change: {
-          $val: function() {
-            cnt++
-          },
-          $defer: {
-            $val: function( emit, event, defer ) {
-              defer.$cancel()
+    it( 'clear inProgress on cancel', function() {
+      var cnt = 0
+      var a = new Observable({
+        $on: {
+          $change: {
+            $val: function() {
+              cnt++
+            },
+            $defer: {
+              $val: function( emit, event, defer ) {
+                defer.$cancel()
+              }
             }
           }
         }
-      }
+      })
+      a.$val = 'hello'
+      expect(a.$on.$change.$defer.$inProgress).to.be.null
+      expect(cnt).to.equal(0)
     })
-    a.$val = 'hello'
-    expect(a.$on.$change.$defer.$inProgress).to.be.null
-    expect(cnt).to.equal(0)
+
   })
 
 })
+
+//now instances -- but first unify the orginator with binds in emitter
