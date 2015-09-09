f0fc29dd90c3fe6dbc4ed2cfd5f8cbaf130a1f2d
Youri Daamen <me@youz.io>
2015-09-09 18:24:33 +0200

    basic subscription

--- test/common/observable/subscribe/index.js
+++ test/common/observable/subscribe/index.js
@@ -1,8 +1,12 @@
 var Observable = require('../../../../lib/observable')
-
+var count
 console.clear()
 
-describe('subscribe on field', function() {
+beforeEach(function(){
+	count = 0
+})
+
+describe('subscribe on existing field', function() {
 
 	it( 'should fire once', function() {
 		var a = new Observable({
@@ -12,14 +16,116 @@ describe('subscribe on field', function() {
 		a.subscribe({
 			aField:true
 		},function(){
-			console.log('go!go')
+			count++
 		})
 
 		a.aField.$val = 2
 
+		expect(count).equals(1)
+
 	})
 
 })
+
+describe('subscribe on non-existent field', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable()
+
+		a.subscribe({
+			aField:true
+		},function(){
+			count++
+		})
+
+		a.set({aField:1})
+
+		a.aField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on existent nested field', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable({
+			aField:{
+				anotherField:1
+			}
+		})
+
+		a.subscribe({
+			aField:{
+				anotherField:true
+			}
+		},function(){
+			count++
+		})
+
+		a.aField.anotherField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on non-existent nested field in existing field', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable({
+			aField:true
+		})
+
+		a.subscribe({
+			aField:{
+				anotherField:true
+			}
+		},function(){
+			count++
+		})
+
+		a.aField.set({anotherField:true})
+
+		a.aField.anotherField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+describe('subscribe on non-existent nested field in non-existent field', function() {
+
+	it( 'should fire once', function() {
+		var a = new Observable()
+
+		a.subscribe({
+			aField:{
+				anotherField:true
+			}
+		},function(){
+			count++
+		})
+
+		a.set({
+			aField:{
+				anotherField:true
+			}
+		})
+
+		a.aField.anotherField.$val = 2
+
+		expect(count).equals(1)
+
+	})
+
+})
+
+
 
 //op subsEmitter komt fire waar je een listener aan kan meegeven en een target
 
