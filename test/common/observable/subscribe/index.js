3d0cea214b814c00b45775edf849c07ec1e16526
Youri Daamen <me@youz.io>
2015-09-09 16:10:25 +0200

    subscribe

--- test/common/observable/subscribe/index.js
+++ test/common/observable/subscribe/index.js
@@ -1,6 +1,52 @@
-require('./subsemitter-emitloop')
-require('./subscribe-by-key')
-require('./subscribe-by-any')
-require('./subscribe-over-references')
-require('./subscribe-over-parent')
-require('./subscribe-with-instances')
+var Observable = require('../../../../../lib/observable')
+
+describe('subscribe on existing field',function(){
+
+	var a = new Observable({
+		aField:1
+	})
+
+	a.subscribe({
+		aField:true	
+	},function(){
+		console.log('go!')
+	},'special')
+
+	// a.subscribe({
+	// 	"*":true //any
+	// 	"&":true //deep
+	// 	somefield:true //field
+	// },'aField')
+
+})
+
+describe('subscribe on non-existing field',function(){
+
+	var a = new Observable()
+	a.subscribe({
+		aField:true	
+	})
+
+})
+
+describe('subscribe on existing ref field',function(){
+
+	var a = new Observable({
+		aField:1
+	})
+	var b = new Observable(a)
+	b.subscribe({
+		aField:true	
+	})
+
+})
+
+describe('subscribe on non-existing ref field',function(){
+
+	var a = new Observable()
+	var b = new Observable(a)
+	b.subscribe({
+		aField:true	
+	})
+
+})
