1c816510e9c4f6d0d1119a51f6bc07529a468015
rick smit <rick@vigour.io>
2015-09-20 13:30:37 +0200

    added flatten option for convert

--- test/common/methods/convert/index.js
+++ test/common/methods/convert/index.js
@@ -1,55 +1,52 @@
-var Base = require('../../../../lib/base/');
+var Base = require('../../../../lib/base/')
 
-// require('../../../../lib/')
 console.clear()
 
 Base.prototype.inject(
   require('../../../../lib/methods/convert')
-);
+)
 
-describe('convert', function() {
-  var a;
-
-  beforeEach(function() {
+describe('convert', function () {
+  var a
+  beforeEach(function () {
     a = new Base({
       $key: 'a',
       x: {
         y: 123
       }
-    });
-  });
+    })
+  })
 
-  it('should convert and be the deep equal to expected object', function() {
-    var convertedObj = a.convert();
+  it('should convert and be the deep equal to expected object', function () {
+    var convertedObj = a.convert()
     var expectedObject = {
       x: {
         y: {
           $val: 123
-        },
-      },
-    };
+        }
+      }
+    }
 
-    expect(convertedObj).to.eql(expectedObject);
-  });
-
-  it('should output normal object', function() {
-    var convertedObj = a.convert({
-      plain: true
-    });
-    expect(convertedObj).to.be.an('object');
-  });
-
-  it('should convert function to string', function() {
-    var convertedObj = a.convert({
-      string: true
-    });
-    expect(convertedObj).to.be.an('string');
-
+    expect(convertedObj).to.eql(expectedObject)
   })
 
-  it('should exclude key/value', function() {
+  it('should output normal object', function () {
     var convertedObj = a.convert({
-      exclude: function(val) {
+      plain: true
+    })
+    expect(convertedObj).to.be.an('object')
+  })
+
+  it('should convert function to string', function () {
+    var convertedObj = a.convert({
+      string: true
+    })
+    expect(convertedObj).to.be.an('string')
+  })
+
+  it('should exclude key/value', function () {
+    var convertedObj = a.convert({
+      exclude: function (val) {
         if (val === 'y') {
           return true
         }
@@ -58,8 +55,9 @@ describe('convert', function() {
     expect(convertedObj.x.y).to.be.undefined
   })
 
-  //fnToString
-  //exclude
-  //plain
-
-});
+  it('should do stuff with flatten', function () {
+    var convertedObj = a.convert({
+      flatten: true
+    })
+  })
+})
--- test/common/utils/flatten.js
+++ test/common/utils/flatten.js
@@ -7,24 +7,22 @@ describe('flatten', function () {
   it('should flatten, duh'
   , function () {
     var expected = {
-      'a/b/c/e': 1,
-      'a/b/c/f': 2,
-      'a/b/c/g': 3
+      'a.b.c.e': 1,
+      'a.b.c.f': 2,
+      'a.b.c.g': 3
     }
     var flattened = flatten(original)
-
     expect(flattened).to.deep.equal(expected)
   })
 
   it('should flatten accept an optional separator argument'
   , function () {
     var expected = {
-      'a.b.c.e': 1,
-      'a.b.c.f': 2,
-      'a.b.c.g': 3
+      'a/b/c/e': 1,
+      'a/b/c/f': 2,
+      'a/b/c/g': 3
     }
-    var flattened = flatten(original, '.')
-
+    var flattened = flatten(original, '/')
     expect(flattened).to.deep.equal(expected)
   })
 })
