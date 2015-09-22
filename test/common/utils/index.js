c45e2f507fceb9e390b3bfbf6628419b236528f2
Jim de Beer <jim@vigour.io>
2015-09-22 19:17:36 +0200

    cleaned up utils test

--- test/common/utils/index.js
+++ test/common/utils/index.js
@@ -4,11 +4,8 @@ describe('util', function () {
 
   describe('isNumber', function () {
     it('should check if parameter is a number and return a boolean value', function () {
-      // must pass
       expect(util.isNumber(2)).to.be.ok
       expect(util.isNumber(-10)).to.be.ok
-
-      // must fail
       expect(util.isNumber('2')).to.not.be.ok
       expect(util.isNumber('two')).to.not.be.ok
     })
@@ -43,15 +40,11 @@ describe('util', function () {
       var testFunc = function () {
         return arguments
       }
-
-      // must pass
       expect(
         Array.isArray(
           util.convertToArray(testFunc(1, 2, 3, 4))
         )
       ).to.be.true
-
-      // must fail
       expect(
         Array.isArray(
           testFunc(1, 2, 3, 4)
