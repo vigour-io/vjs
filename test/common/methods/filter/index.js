25f161ab0695f0e26e026e35f7246497e0a4a4c2
Jim de Beer <jim@vigour.io>
2015-09-22 20:30:28 +0200

    fixed filter test

--- test/common/methods/filter/index.js
+++ test/common/methods/filter/index.js
@@ -5,8 +5,7 @@ Base.prototype.inject(Filter)
 
 describe('filter', function () {
   it('should filter Array', function () {
-    var filter = Filter.$define.filter
-
+    var filter = Filter.define.filter
     var result = filter.call([3, 2, 23, 10, 50], function (item) {
       return item % 2 === 0
     })
@@ -23,26 +22,22 @@ describe('filter', function () {
         z: 123,
         x: {
           ab: 456,
-          aa: 789,
+          aa: 789
         }
       })
     })
 
     it('should filter values of Base nested properties', function () {
       var result = a.filter(123)
-
       expect(result).to.have.length(1)
       expect(result[0]).to.be.eql(a.z)
     })
 
     it('should be possible to pass an array of matching elements', function () {
       var result = a.x.filter([456, 789])
-
       expect(result).to.have.length(2)
       expect(result[0]).to.eql(a.x.ab)
       expect(result[1]).to.eql(a.x.aa)
     })
-
   })
-
 })
