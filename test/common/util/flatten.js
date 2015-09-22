a64b3879c9f5ac69be7785ceef9a8cddb47674e3
Jim de Beer <jim@vigour.io>
2015-09-22 19:46:26 +0200

    use . instead of / as default seperator character

--- test/common/util/flatten.js
+++ test/common/util/flatten.js
@@ -4,9 +4,9 @@ describe('flatten', function () {
   it('should flatten'
     , function () {
       var expected = {
-        'a/b/c/e': 1,
-        'a/b/c/f': 2,
-        'a/b/c/g': 3
+        'a.b.c.e': 1,
+        'a.b.c.f': 2,
+        'a.b.c.g': 3
       }
       var flattened = flatten(original)
 
@@ -16,11 +16,11 @@ describe('flatten', function () {
   it('should flatten accept an optional separator argument'
     , function () {
       var expected = {
-        'a.b.c.e': 1,
-        'a.b.c.f': 2,
-        'a.b.c.g': 3
+        'a/b/c/e': 1,
+        'a/b/c/f': 2,
+        'a/b/c/g': 3
       }
-      var flattened = flatten(original, '.')
+      var flattened = flatten(original, '/')
 
       expect(flattened).to.deep.equal(expected)
     })
