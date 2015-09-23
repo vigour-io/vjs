da116ae4996c613d22ccd7a1988b90b96a7b1175
Jim de Beer <jim@vigour.io>
2015-09-23 13:03:10 +0200

    redid tostring test (new exclude order)

--- test/common/methods/tostring/index.js
+++ test/common/methods/tostring/index.js
@@ -8,7 +8,7 @@ describe('toString', function () {
 
   beforeEach(function () {
     a = new Base({
-      $key: 'a',
+      key: 'a',
       x: {
         y: 123
       }
@@ -19,16 +19,14 @@ describe('toString', function () {
     expect(a.toString()).to.eql('{\n  "x": {\n    "y": 123\n  }\n}')
   })
 
-  it('should include $val fields if second argument is true', function () {
-    expect(a.toString(false, true)).to.eql('{\n  "x": {\n    "y": {\n      "$val": 123\n    }\n  }\n}')
+  it('should include val fields if second argument is true', function () {
+    expect(a.toString(false, true)).to.eql('{\n  "x": {\n    "y": {\n      "val": 123\n    }\n  }\n}')
   })
 
   it('should exclude some properties when passed custom exclude', function () {
-    var result = a.toString(function (key) {
-      return key === 'y'
+    var result = a.toString(function (property, key) {
+      return key === 'y' || a._properties[key]
     })
-
-    // this exclude brings properties beginning with '$'
     expect(result).to.eql('{\n  "x": {}\n}')
   })
 
