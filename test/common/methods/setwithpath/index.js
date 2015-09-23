17c147975a76031050793a55f009688423fa788d
Jim de Beer <jim@vigour.io>
2015-09-23 13:00:29 +0200

    redid path tests, fixed small bug - reneamed method tests without "-"

--- test/common/methods/setwithpath/index.js
+++ test/common/methods/setwithpath/index.js
@@ -8,32 +8,28 @@ describe('setWithPath', function () {
 
   beforeEach(function () {
     a = new Base({
-      $key: 'a'
+      key: 'a'
     })
   })
 
-  it('should set and get the $val', function () {
+  it('should set and get the val', function () {
     var result = a.setWithPath('x.y.z', {
-      $val: 'rahh'
+      val: 'rahh'
     })
-
-    expect(a.x.y.z.$val).to.eql('rahh')
+    expect(a.x.y.z.val).to.eql('rahh')
     expect(result).to.eql(a.x.y.z)
   })
 
   it('should be able to set the $val within path string', function () {
-    var result = a.setWithPath('j.k.l.$val', 'rahh')
-
-    expect(a.j.k.l.$val).to.eql('rahh')
+    var result = a.setWithPath('j.k.l.val', 'rahh')
+    expect(a.j.k.l.val).to.eql('rahh')
     expect(result).to.eql('rahh')
   })
 
   it('should set the path but be empty when set is not defined', function () {
-    var result = a.setWithPath('o.p.q')
-
+    a.setWithPath('o.p.q')
     expect(a.o.p.q).to.be.defined
     expect(a.o.p.q).to.be.instanceOf(Base)
-    expect(a.o.p.q._$input).to.be.undefined
+    expect(a.o.p.q._input).to.be.undefined
   })
-
 })
