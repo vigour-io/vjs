81297c398615c9d99c700963a2eac74cfdc0a846
Jim de Beer <jim@vigour.io>
2015-09-24 14:47:00 +0200

    - more tests for get method - better tests for property

--- test/common/base/property.js
+++ test/common/base/property.js
@@ -1,5 +1,7 @@
 var Base = require('../../../lib/base')
 
+// console.clear()
+
 describe('properties', function () {
   it('add and set basic property', function () {
     var a = new Base({
@@ -24,25 +26,40 @@ describe('properties', function () {
   })
 
   describe('custom overides', function () {
-    var a
+    var a, b, c
     it('add and set base property on a custom field (constructor)', function () {
       var rick = new Base()
       a = new Base({
         key: 'a',
         properties: {
-          rick: { val: rick, overide: '$rick' }
+          rick: { val: rick, overide: '_rick' }
         }
       })
       a.set({ rick: 'dope' })
-      expect(a.$rick.val).equals('dope')
-      expect(a.$rick).instanceof(rick.Constructor)
+      expect(a._rick.val).equals('dope')
+      expect(a._rick).instanceof(rick.Constructor)
     })
 
-    it('create and instance, have correct context', function () {
-      var b = new a.Constructor({
+    it('create an instance of a (b), have correct context', function () {
+      b = new a.Constructor({
         key: 'b'
       })
-      expect(b.$rick.path[0]).equals('b')
+      expect(b._rick.path[0]).equals('b')
+    })
+
+    it('create an instance of b (c), have correct context', function () {
+      c = new b.Constructor({
+        key: 'c'
+      })
+      expect(c._rick.path[0]).equals('c')
+    })
+
+    it('set rick on c', function () {
+      c = new b.Constructor({
+        key: 'c',
+        rick: 'hello'
+      })
+      expect(c._rick).not.equals(b._rick)
     })
   })
 })
--- test/common/methods/get/index.js
+++ test/common/methods/get/index.js
@@ -8,11 +8,14 @@ describe('get', function () {
 
   beforeEach(function () {
     a = new Base({
+      key: 'a',
       x: {
         y: 123
       },
       z: 456
     })
+    a.random = function () {}
+    a.random.field = true
   })
 
   it('should get a field passing a its name', function () {
@@ -26,5 +29,14 @@ describe('get', function () {
   it('should get undefined passing unexisting path or field', function () {
     expect(a.get('y')).to.be.undefined
     expect(a.get(['x', 'z'])).to.be.undefined
+  })
+
+  it('should not work for strings', function () {
+    expect(a.get(['key', 'nope'])).to.be.undefined
+  })
+
+  it('should work for functions', function () {
+    expect(a.get(['random', 'nope'])).to.be.undefined
+    expect(a.get(['random', 'field'])).equals(true)
   })
 })
