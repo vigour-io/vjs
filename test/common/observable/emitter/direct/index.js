97aad405c4cd1d0afc318bde41ca8a151a0ca07a
Jim de Beer <jim@vigour.io>
2015-09-25 16:22:31 +0200

    added instances

--- test/common/observable/emitter/direct/index.js
+++ test/common/observable/emitter/direct/index.js
@@ -2,79 +2,10 @@ describe('direct', function () {
   require('./listeners')
   require('./references')
   require('./attach')
+  require('./set')
 })
 
-//
-//   it('use $block on a new observable --> obs4', function () {
-//     var cnt = 0
-//     var cnt2 = 0
-//
-//     var obs4 = new Observable({
-//       $key: 'obs4',
-//       specialField: {
-//         $on: {
-//           $change: function () {
-//             expect(this.$val).msg('specialField').to.equal('hello')
-//             cnt2++
-//           }
-//         }
-//       },
-//       $on: {
-//         $change: function (event) {
-//           cnt++
-//           event.$block = true
-//
-//           this.set({
-//             specialField: 'xxxx',
-//             letsSee: true
-//           }, event)
-//
-//           this.set({
-//             specialField: 'hello'
-//           }, event)
-//
-//           event.$block = null
-//         }
-//       }
-//     })
-//
-//     expect(cnt).msg('obs4 listener fired').to.equal(0)
-//     expect(cnt2).msg('specialField fired').to.equal(0)
-//
-//     obs4.set({ hello: true })
-//
-//     expect(cnt).msg('obs4 listener fired').to.equal(1)
-//     expect(cnt2).msg('specialField fired').to.equal(1)
-//
-//   })
-//
-//   it('change nested fields , fire correct emitters', function () {
-//     var measure = {
-//       a: 0,
-//       x: 0
-//     }
-//     var a = new Observable({
-//       $key: 'a',
-//       $on: {
-//         $change: function () {
-//           measure.a++
-//         }
-//       },
-//       x: {
-//         $on: {
-//           $change: function ( event, meta ) {
-//             measure.x++
-//           }
-//         }
-//       }
-//     })
-//     a.set({
-//       x: true
-//     })
-//     expect(measure.a).to.equal(0)
-//     expect(measure.x).to.equal(1)
-//   })
-//
+// ---- !!!TO REMOVE !!!-----
 //   it('should emit change event when property is removed due to ' +
 //   'parent / ancestor properties being removed',
 //     function () {
---
+++ test/common/observable/emitter/instances/set.js
@@ -0,0 +1,45 @@
+describe('set on instance nested field', function () {
+  var Observable = require('../../../../../lib/observable')
+
+  var cnt = 0
+
+  var a = new Observable({
+    key: 'a',
+    trackInstances: true,
+    b: {
+      c: {
+        on: {
+          change: function () {
+            cnt++
+          }
+        }
+      }
+    }
+  })
+
+  var aInstance
+
+  it('listener fires once', function () {
+    aInstance = new a.Constructor({
+      key: 'aInstance'
+    })
+    expect(cnt).to.equal(0)
+  })
+
+  it('listener fired 2 times', function () {
+    a.b.set({
+      c: true
+    })
+    expect(cnt).to.equal(2)
+  })
+
+  it('listener fired 3 times', function () {
+    //
+    aInstance.b.set({
+      c: '?'
+    })
+    // we are now doing resolving of context!
+    expect(cnt).to.equal(3)
+  })
+
+})
