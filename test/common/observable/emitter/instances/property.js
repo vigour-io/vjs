cdde8d7e66c08652ab3eec876404fdf784021e62
Jim de Beer <jim@vigour.io>
2015-09-28 19:49:26 +0200

    added property instances test in emitter

---
+++ test/common/observable/emitter/instances/property.js
@@ -0,0 +1,29 @@
+describe('property listener', function () {
+  var Observable = require('../../../../../lib/observable')
+  var cnt = 0
+  var a = new Observable({
+    key: 'a',
+    b: {},
+    on: {
+      property: function (event, meta) {
+        cnt++
+        console.log('property fires!', meta, this.path)
+      }
+    }
+  })
+  var aInstance
+
+  it('create instance, listener does not fire', function () {
+    aInstance = new a.Constructor({
+      key: 'aInstance'
+    })
+    expect(cnt).to.equal(0)
+  })
+
+  it('listener fired 2 times', function () {
+    a.set({
+      c: true
+    })
+    expect(cnt).to.equal(2)
+  })
+})
