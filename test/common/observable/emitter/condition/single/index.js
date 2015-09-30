2b6b94d332347a822206aad419f560ba2cb73ac2
Jim de Beer <jim@vigour.io>
2015-09-30 15:05:07 +0200

    starting condition plan

--- test/common/observable/emitter/condition/single/index.js
+++ test/common/observable/emitter/condition/single/index.js
@@ -11,6 +11,10 @@ describe('single instance', function () {
           },
           val: function () {
             console.error('--->hey do it!')
+            // now have to think of how to exclude from operators
+            // maybe the emits is the best way
+            // handle multuple events at the same time -- then its easy
+            // if we use the binds[stamp] thing thats
             done()
           }
         }
@@ -27,4 +31,19 @@ describe('single instance', function () {
   hands in the this to parseValue and continiues from there
   -- preferbly the this .input
   -- condition is responsible for setting .output
+*/
+
+/*
+ setup of condition on emit instead of trigger
+ makes it easier to get different inputs per condition
+ condition can be called within instances (contexts are not important!)
+ so condition is more of a thing in emitter.push ???? insteas of trigger
+ what about instances that have litterly no difference?
+
+  make bind work for this (bind is nice if it works a bit better)
+  operators -- need to exclude input in parseValue
+  batch option
+  operators --- operators are a bit different the don work on the ouput
+  make this an option as well (ignore input/ouput)
+  bind for emitter, en parse functies
 */
