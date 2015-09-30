337dee43ae2cfc3664d7b8f109ae662a185af575
Jim de Beer <jim@vigour.io>
2015-09-30 14:58:05 +0200

    start of new condition

--- test/common/observable/emitter/condition/single/index.js
+++ test/common/observable/emitter/condition/single/index.js
@@ -5,13 +5,20 @@ describe('single instance', function () {
     var a = new Observable({
       on: {
         data: {
-          condition: function () {
+          condition: function (complete, event) {
             console.log('should do something...')
+            setTimeout(complete, 1000)
+          },
+          val: function () {
+            console.error('--->hey do it!')
             done()
           }
         }
       }
     })
+
+    console.error('no way gozee')
+    a.val = 'a change!'
   })
 })
 
