34033e19b3caa5e39b2e2e740c76ceae417a65a1
Jim de Beer <jim@vigour.io>
2015-10-01 15:40:22 +0200

    start of operator parse val -- unify with base later

--- examples/operators/index.js
+++ examples/operators/index.js
@@ -1,14 +1,17 @@
 var Operator = require('../../lib/operator')
 console.log(Operator)
 var bla = new Operator()
-
 console.log('dd', bla)
 
-var Obs = require('../../lib/observable')
-Obs.prototype.inject(require('../../lib/operator/inject'))
 
-var a = new Obs({
- val:1,
- $add:2
+
+
+
+var Cnstr = require('../../lib/base')
+Cnstr.prototype.inject(require('../../lib/operator/inject'))
+
+var a = new Cnstr({
+ val: 1,
+ $add: 2
 })
-console.log('ok ok', a, a.val )
+console.log('ok ok', a, a.val)
