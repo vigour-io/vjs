177e4fa56701d46b830a88d6d7ac062c5671fef2
Jim de Beer <jim@vigour.io>
2015-10-01 16:19:58 +0200

    split up in multiple files, added operators

--- examples/operators/index.js
+++ examples/operators/index.js
@@ -4,8 +4,8 @@ var bla = new Operator()
 console.log('dd', bla)
 
 
-
-
+//make it work only with obs first?
+// why not...
 
 var Cnstr = require('../../lib/base')
 Cnstr.prototype.inject(require('../../lib/operator/inject'))
@@ -14,4 +14,6 @@ var a = new Cnstr({
  val: 1,
  $add: 2
 })
+console.log('ok ok', a, a.val)
+a.set({ $add: 10 })
 console.log('ok ok', a, a.val)
