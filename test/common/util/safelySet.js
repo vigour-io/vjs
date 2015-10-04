ca6ae03a583658be9d47c3f535e97e73fd224444
Youri Daamen <me@youz.io>
2015-10-04 19:40:01 +0200

    updated escaping fields

--- test/common/util/safelySet.js
+++ test/common/util/safelySet.js
@@ -1,73 +1,5 @@
 var Base = require('../../../lib/base')
 var Observable = require('../../../lib/observable')
-var util = require('../../../lib/util')
-var isPlainObj = util.isPlainObj
-
-// Base.prototype.define({
-//   safelySet: function (val, event, nocontext) {
-//     var base = this
-//     var resolveContext = !nocontext && base._context
-//     if (isPlainObj(val)) {
-//       if (resolveContext) {
-//         base = base.resolveContext(val, event)
-//       } else {
-//         var changed
-//         for (var key in val) {
-//           if (base._input === null) {
-//             break
-//           }
-//           if (key === 'val') {
-//             if (base.setValue(val[key], event, resolveContext)) {
-//               changed = true
-//             }
-//           } else {
-//             if (base.safelySetKey(key, val[key], event, nocontext)) {
-//               changed = true
-//             }
-//           }
-//         }
-//         if (!changed) {
-//           return
-//         }
-//       }
-//     } else {
-//       base = base.setValue(val, event, resolveContext)
-//     }
-//     return base
-//   },
-//   safelySetKey: function (key, val, event, nocontext) {
-//     var property = this[key]
-//     if (this.properties[key] || property && !property.Constructor) {
-//       key = 'escaped_' + key
-//       property = this[key]
-//     }
-//     // return this.safelySetKeyInternal(key, val, this[key], event, nocontext)
-//     if (property) {
-//       if (property._parent !== this) {
-//         if (val === null) {
-//           this[key] = null
-//           return this
-//         } else {
-//           var Constructor = property.Constructor
-//           this[key] = new Constructor(void 0, false, this, key)
-//           this[key].safelySet(val, event)
-//           return this[key]
-//         }
-//       } else {
-//         property.safelySet(val, event, nocontext)
-//         return
-//       // double check if not returning anything is the best choice
-//       }
-//     } else {
-//       if (val !== null) {
-//         this.addNewProperty(key, val, property, event)
-//         return this
-//       } else {
-//         return
-//       }
-//     }
-//   }
-// })
 
 describe('safelySet on Base', function () {
   it('should store the reserved fields elsewhere', function () {
@@ -87,7 +19,6 @@ describe('safelySet on Base', function () {
       },
       on: false
     }, void 0, void 0, true)
-    console.log(JSON.stringify(test.convert(), false, 2))
   })
 })
 
@@ -112,6 +43,17 @@ describe('safelySet on Observable', function () {
       },
       on: false
     }, void 0, void 0, 'test_')
-    console.log(JSON.stringify(test.convert(), false, 2))
   })
 })
+
+// function startsWith (str, start) {
+//   var i = 0
+//   var letter = start[i]
+//   while (letter) {
+//     if (letter !== str[i]) {
+//       return false
+//     }
+//     letter = start[++i]
+//   }
+//   return true
+// }
