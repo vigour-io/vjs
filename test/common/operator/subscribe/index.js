04802481aac65c5ac8878f1842abc81bb503c60a
Youri Daamen <me@youz.io>
2015-10-05 13:55:06 +0200

    api'ing subscribe and bind

---
+++ test/common/operator/subscribe/index.js
@@ -0,0 +1,77 @@
+describe('subscribe and bind', function () {
+  var Observable = require('../../../../lib/observable')
+
+  it('create a new base inject operators', function () {
+    var child = new Observable({
+      inject: require('../../../../lib/operator/subscribe'),
+      key: 'a',
+      // val: 'hello',
+      $subscribe: {
+        upward: {
+          title: true
+        }
+      }
+    })
+
+    // child.$subscribe.output = new Observable(1)
+    child.$subscribe.emit('data')
+
+    var parent = new Observable({
+      title: 'myTitle',
+      a: {
+        useVal: child
+      }
+    })
+
+    expect(child.val).equals('myTitle')
+  })
+})
+
+// describe('subscribe and bind', function () {
+//   var Observable = require('../../../../lib/observable')
+//   it('create a new base inject operators', function () {
+//     var child = new Observable({
+//       inject: require('../../../../lib/operator/subscribe'),
+//       key: 'a',
+//       // val: 'hello',
+//       $subscribe: {
+//         parent: {
+//           title: true
+//         }
+//       }
+//     })
+
+//     var parent = new Observable({
+//       title: 'myTitle',
+//       a: {
+//         useVal: child
+//       }
+//     })
+
+//     expect(child.val).equals('myTitle')
+//   })
+// })
+
+var obs = new Observable({
+  $data: {
+    title: true,
+    description: true
+  }
+})
+
+var obs = new Observable({
+  title: {
+    $data: 'title'
+  },
+  subtitle: {
+    $data: 'subtitle'
+  }
+})
+
+var obs = new Observable({
+  $data: 'info',
+  $filter: {
+    title: true,
+    subtitle: true
+  }
+})
