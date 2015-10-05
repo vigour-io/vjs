7877f8a315904fb55a65b2f046012c5d10a76581
Youri Daamen <me@youz.io>
2015-10-05 20:58:34 +0200

    added the subscribe operator

--- test/common/operator/subscribe/index.js
+++ test/common/operator/subscribe/index.js
@@ -1,26 +1,11 @@
+var Observable = require('../../../../lib/observable')
+Observable.prototype.inject(require('../../../../lib/operator/subscribe'))
+
 describe('subscribe and bind', function () {
-  var Observable = require('../../../../lib/observable')
-
-  it('create a new base inject operators', function () {
+  it('subscribe it', function () {
     var child = new Observable({
-      inject: require('../../../../lib/operator/subscribe'),
       key: 'a',
-      $subscribe: {
-        upward: {
-          title: true
-        }
-      }
-    })
-
-    // child.$subscribe.output = new Observable(1)
-    // child.$subscribe.emit('data')
-    child.subscribe({
-      upward: {
-        title: true
-      }
-    }, function () {
-      this.$subscribe.output = 'yeeey'
-    // console.log('struggles!', this)
+      $subscribe: 'parent.title'
     })
 
     var parent = new Observable({
@@ -30,82 +15,45 @@ describe('subscribe and bind', function () {
       }
     })
 
+    expect(parent)
     expect(child.val).equals('myTitle')
   })
 })
 
-// describe('subscribe and bind', function () {
-//   var Observable = require('../../../../lib/observable')
-//   it('create a new base inject operators', function () {
-//     var child = new Observable({
-//       inject: require('../../../../lib/operator/subscribe'),
-//       key: 'a',
-//       // val: 'hello',
-//       $subscribe: {
-//         parent: {
-//           title: true
-//         }
-//       }
-//     })
+describe('subscribe and bind, instance', function () {
+  it('subscribe it', function () {
+    var child = new Observable({
+      key: 'a',
+      $subscribe: 'parent.title'
+    })
 
-//     var parent = new Observable({
-//       title: 'myTitle',
-//       a: {
-//         useVal: child
-//       }
-//     })
+    var parent = new Observable({
+      title: 'myTitle',
+      a: {
+        useVal: new child.Constructor()
+      }
+    })
 
-//     expect(child.val).equals('myTitle')
-//   })
-// })
+    expect(parent)
+    expect(parent.a.val).equals('myTitle')
+  })
+})
 
-// var obs = new Observable({
-//   $data: {
-//     title: true,
-//     description: true
-//   }
-// })
+describe('subscribe and bind nested field', function () {
+  it('subscribe it', function () {
+    var child = new Observable({
+      key: 'a',
+      $subscribe: 'field.nested.title'
+    })
 
-// var obs = new Observable({
-//   title: {
-//     $data: 'title'
-//   },
-//   subtitle: {
-//     $data: 'subtitle'
-//   }
-// })
-
-// var obs = new Observable({
-//   $data: 'info',
-//   $filter: {
-//     title: true,
-//     subtitle: true
-//   }
-// })
-
-/*
-var list = new Element({
-  collection: {
-    element: new Element({
-      title: {
-        text: {
-          $subscribe: 'upward.title'
+    child.set({
+      field: {
+        nested: {
+          title: 'myTitle'
         }
       }
     })
-  }
-})
-*/
 
-/*
-var list = new Element({
-  ChildConstructor: new Element({
-    title: {
-      text: {
-        $data:'title'
-      }
-    }
-  }),
-  $subscribe:'upward.$data.shows'
+    expect(child.val).equals('myTitle')
+  })
 })
-*/
