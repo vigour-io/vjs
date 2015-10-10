034b3f201a4827d9226a1fd7fd977328c5700be7
rick smit <rick@vigour.io>
2015-10-10 16:49:43 +0200

    fixed plain

--- test/common/observable/tracking/index.js
+++ test/common/observable/tracking/index.js
@@ -1,28 +1,26 @@
 // var Observable = require('../../../../lib/observable/')
 // var tracking = require('../../../../lib/tracking/')
-// var trackerEmitter = require('../../../../lib/tracking/emitter')
+var trackerEmitter = require('../../../../lib/tracking/emitter')
 var Event = require('../../../../lib/event/')
 
 // trackerEmitter.inject(require('../../../../lib/tracking/service/log'))
 // this is just to log stuff (.toString yields nicer result for events)
-trackerEmitter.inject(require('../../../../lib/tracking/service/log'))
+// trackerEmitter.inject(require('../../../../lib/tracking/service/log'))
 Event.prototype.inject(require('../../../../lib/event/toString'))
 // parent and error are still weird
 
 Event.prototype.inject(require('../../../../lib/event/toString'))
 describe('Direct tracking', function () {
   describe('Default', function () {
-    require('./default')
+    require('./default.js')
   })
   describe('Objects', function () {
-    require('./object')
+    require('./object.js')
   })
   describe('Arrays', function () {
-    require('./array')
+    require('./array.js')
   })
   describe('Strings', function () {
-    require('./string')
+    require('./string.js')
   })
 })
-
-console.clear()
