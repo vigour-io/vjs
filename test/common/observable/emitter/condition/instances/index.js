f8e0a9d02dd253d7730f6c5c8202dfdf97e51c1f
Jim de Beer <jim@vigour.io>
2015-10-10 16:36:41 +0200

    cleanup condition tests

--- test/common/observable/emitter/condition/instances/index.js
+++ test/common/observable/emitter/condition/instances/index.js
@@ -6,7 +6,7 @@ describe('instances', function () {
       on: {
         data: {
           condition: function (data, done, event) {
-            setTimeout(done, 1000)
+            setTimeout(done, 10)
           },
           val: function () {
             cnt++
@@ -19,13 +19,12 @@ describe('instances', function () {
     })
     var b = new a.Constructor()
     a.val = 'a change!'
-    // fire for b and a
   })
 })
 
 describe('context', function () {
   var Observable = require('../../../../../../lib/observable')
-  xit('fires condition trigger', function (done) {
+  it('fires condition trigger', function (done) {
     var cnt = 0
     var a = new Observable({
       key:'a',
@@ -45,158 +44,8 @@ describe('context', function () {
       }
     })
 
-    //ways set all queed? how to do
-
-    // prob best to do condition per bind
-    // then share stuff later (when same etc)
-    //
-
-    //also support condition in on syntx
-    // on('data', fn, condition) //conditions per fn instead of emitter?
-    //that is pretty wild
-    // needs a very easy subscribe
-    // basicly you always want to listen to all properties for a condtion
-    // when you set bind takes this into account in subscription!
     var b = new a.Constructor({time: 300})
     var c = new b.Constructor({time: 500})
     a.val = 'a change!'
-    // ignore all that are not part of this chain
-    // same for all normal events
-    // fire for b and a
   })
 })
-
-//contet a a' a''      -- sharen a.data kan zo zijn dat de value different is
-
-// dit betekent dat we eerst de value getter gaan runnen
-// als de val hetzelfde is van de input (however the fuck were going to figure that out)
-// dan shared ie met zn corresponding value hash
-
-
-/*
-  few things
-
-  input val moet worden gecomminicate
-  -- dit het total input object wat / val wat responsible is
-
-  hoe doe je deze?
-
-  2 opties
-  - 1 je moet een result obj ervan maken in condition
-  // dit obj word gestringified en hashed
-  // dit obj word ge cross checked met je instances etc (most efficient)
-
-  // zou hetzelfde system moeten usen als de normale operator check
-
-  // idee word dan dat operators val get goed werkt met context / instances
-
-  // als we dat kunnen unifyen hebben we een sluitend systeem
-
-
-
-  var a = new obs({
-    input: {
-      $type: 'string'
-    },
-    $transform: String.toUpperCase,
-    on: {
-      data: {
-        condition: function(data) {
-          //parsed for before
-          http.get(data)
-        }
-      }
-    }
-  })
-
-
-  //operators hebben defaults voor input or ouput
-  //this is all very specific for the on data condition...
-  //e.g subsandbind operator is by default input
-  //transform default is output
-
-  var a = new obs({
-    input: {
-      $type: 'string'
-    },
-    output: {
-      $transform: String.toUpperCase,
-    }
-    on: {
-      data: {
-        condition: function(data, done) {
-          http.get(data, done)
-          //does this.input (parses input)
-        }
-      }
-    }
-  })
-
-  var a = new obs({
-    $type:'string',
-    $transform: String.toUpperCase,
-    on: {
-      data: {
-        condition: {
-          // default data condtion gets is .val (parsed of data)
-          $type:'string',
-          val: function(data) {
-            http.get(data) (something like that)
-          }
-        }
-      },
-      new: {
-        condition: {
-          // check the data by default pass this with new
-          $instanceof: Spesh,
-          val: function(data) {
-            http.get(data) (something like that)
-          }
-        }
-      }
-    }
-  })
-
-  //mesh -- best since we have a format for $subs and data now for example
-  var a = new obs({
-    $input: {
-      $type: 'string'
-    },
-    $transform: String.toUpperCase,
-    on: {
-      data: {
-        condition: {
-          // default data condtion gets is .val (parsed of data)
-          $type:'string',
-          val: function(data) {
-            http.get(data) (something like that)
-          }
-        }
-      },
-      new: {
-        condition: {
-          // check the data by default pass this with new
-          $instanceof: Spesh,
-          val: function(data) {
-            http.get(data) (something like that)
-          }
-        }
-      }
-    }
-  })
-
-//returning void 0 in an operator will cancel all others or something
-
-var a = new Observable({
-  $data: 'path',
-  on: {
-    data: {
-      condition: {
-        $type:'string',
-        //condition: fn .... load some metadata
-      }
-    }
-  }
-})
-
-*/
--- test/common/observable/emitter/condition/single/index.js
+++ test/common/observable/emitter/condition/single/index.js
@@ -43,39 +43,3 @@ describe('single instance', function () {
     a.val = 'aaa'
   })
 })
-
-// dit is eigenlijk niets meer dan een operator + cb
-// support dat ook gewoon in operator (async operator)
-// just chains callbacks
-// of alleen in condition?
-// ook special behaviour voor bijv een andere observable
-// x: x -- only fire if x fires ? zoiets
-// lijkt ook op middle ware thing
-
-/*
-  condition uses parseValue
-  hands in the this to parseValue and continiues from there
-  -- preferbly the this .input
-  -- condition is responsible for setting .output
-
-  child constructor is soort currying van demethids (chain)
-  net zoals operators eigenlijk
-
-  propberen operators dit te geven
-  cb als extra field helemaal prima
-*/
-
-/*
- setup of condition on emit instead of trigger
- makes it easier to get different inputs per condition
- condition can be called within instances (contexts are not important!)
- so condition is more of a thing in emitter.push ???? insteas of trigger
- what about instances that have litterly no difference?
-
-  make bind work for this (bind is nice if it works a bit better)
-  operators -- need to exclude input in parseValue
-  batch option
-  operators --- operators are a bit different the don work on the ouput
-  make this an option as well (ignore input/ouput)
-  bind for emitter, en parse functies
-*/
