98606e1628ce2df444d09cb66445a498e1efcd45
Jim de Beer <jim@vigour.io>
2015-10-12 23:56:08 +0200

    returning a value in each lets each return

--- test/common/operator/direct/object.js
+++ test/common/operator/direct/object.js
@@ -9,7 +9,6 @@ describe('object', function () {
           inject: require('../../../../lib/operator/all'),
           key: 'a',
           b: 'its b',
-          // merge er ook bij
           $add: function () {
             return { c: 'its c' }
           }
@@ -41,33 +40,69 @@ describe('object', function () {
         })
         expect(a.val).equals(a._cache)
       })
+
+      it('should have field a.b', function () {
+        expect(a.val.b._input).equals(a.b)
+      })
+
+      it('should have field a.b.c', function () {
+        expect(a.val).has.property('c')
+          .which.has.property('_input')
+          .which.has.property('_input')
+          .which.equals('its c')
+      })
     })
   })
 
   describe('transform', function () {
     var a
-    it('create a new observable, should return a cache object', function () {
-      a = new Observable({
-        inject: [
-          require('../../../../lib/operator/all'),
-          require('../../../../lib/methods/map')
-        ],
-        key: 'a',
-        b: 'its b',
-        c: 'its c',
-        $transform: function () {
-          return this.map((property, key) => { return { wow: property } })
-        }
+    describe('function', function () {
+      it('create a new observable, should return a cache object', function () {
+        a = new Observable({
+          inject: [
+            require('../../../../lib/operator/all'),
+            require('../../../../lib/methods/map')
+          ],
+          key: 'a',
+          b: 'its b',
+          c: 'its c',
+          $transform: function () {
+            return this.map((property, key) => { return { wow: property } })
+          }
+        })
+        expect(a.val).equals(a._cache)
       })
-      expect(a.val).equals(a._cache)
+
+      it('should have all fields', function () {
+        var arr = []
+        a.val.each((property, key) => {
+          arr.push(property.wow._input)
+        })
+        expect(arr).to.deep.equal([a.b, a.c])
+      })
     })
 
-    it('should have all fields', function () {
-      var arr = []
-      a.val.each((property, key) => {
-        arr.push(property.wow._input)
+    describe('object', function () {
+      it('create a new observable, should return a cache object', function () {
+        a = new Observable({
+          inject: [
+            require('../../../../lib/operator/all'),
+            require('../../../../lib/methods/map')
+          ],
+          key: 'a',
+          b: 'its b',
+          c: 'its c',
+          $transform: {
+            d: 'haha'
+          }
+        })
+        expect(a.val).equals(a._cache)
       })
-      expect(arr).to.deep.equal([a.b, a.c])
+
+      it('should have field', function () {
+        expect(a.val.d).has.property('_input')
+          .which.equals(a.$transform.d)
+      })
     })
   })
 })
