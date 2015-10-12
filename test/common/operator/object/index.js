17a8899b6f68984d1dea66f8ac3035631b9d404c
Jim de Beer <jim@vigour.io>
2015-10-12 23:04:28 +0200

    create references for each field in cache objects first tests for objects

--- test/common/operator/object/index.js
+++ test/common/operator/object/index.js
@@ -2,16 +2,22 @@ describe('object ', function () {
   var Observable = require('../../../../lib/observable')
   var a
 
-  it('create a new observable, use objects, create cache object', function () {
+  it('create a new observable, use objects, test add', function () {
     console.clear()
     a = new Observable({
       inject: require('../../../../lib/operator/all'),
       key: 'a',
       b: 'its b',
+      // merge er ook bij
       $add: function () {
         return { c: 'its c' }
       }
     })
+    //easy ways to maniupulate cache objects
     expect(a.val).equals(a._cache)
+    expect(a.val.b._input).equals(a.b)
+    expect(a.val).to.have.property('c').which.has.property('_input').which.equals('its c')
+
+    console.log(a.val)
   })
 })
