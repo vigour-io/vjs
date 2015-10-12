7eff62dcf76fefbbd695ce0dadf9e4b2c8265375
Jim de Beer <jim@vigour.io>
2015-10-12 23:45:45 +0200

    renamed subscribe tests

--- test/common/operator/direct/object.js
+++ test/common/operator/direct/object.js
@@ -3,27 +3,44 @@ describe('object', function () {
 
   describe('add', function () {
     var a
-    it('create a new observable, should return a cache object', function () {
-      a = new Observable({
-        inject: require('../../../../lib/operator/all'),
-        key: 'a',
-        b: 'its b',
-        // merge er ook bij
-        $add: function () {
-          return { c: 'its c' }
-        }
+    describe('function', function () {
+      it('create a new observable, should return a cache object', function () {
+        a = new Observable({
+          inject: require('../../../../lib/operator/all'),
+          key: 'a',
+          b: 'its b',
+          // merge er ook bij
+          $add: function () {
+            return { c: 'its c' }
+          }
+        })
+        expect(a.val).equals(a._cache)
       })
-      expect(a.val).equals(a._cache)
+
+      it('should have field a.b', function () {
+        expect(a.val.b._input).equals(a.b)
+      })
+
+      it('should have field a.b.c', function () {
+        expect(a.val).has.property('c')
+          .which.has.property('_input')
+          .which.equals('its c')
+      })
     })
 
-    it('should have field a.b', function () {
-      expect(a.val.b._input).equals(a.b)
-    })
-
-    it('should have field a.b.c', function () {
-      expect(a.val).has.property('c')
-        .which.has.property('_input')
-        .which.equals('its c')
+    describe('nested', function () {
+      it('create a new observable, should return a cache object', function () {
+        a = new Observable({
+          inject: require('../../../../lib/operator/all'),
+          key: 'a',
+          b: 'its b',
+          // merge er ook bij
+          $add: {
+            c: 'its c'
+          }
+        })
+        expect(a.val).equals(a._cache)
+      })
     })
   })
 
