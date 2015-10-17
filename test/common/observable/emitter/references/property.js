548a7d2b3cb1757dfe84c52ee37d2956fa2d4cb5
Jim de Beer <jim@vigour.io>
2015-10-17 09:07:49 +0200

    added test for property definition reference problem #195

--- test/common/observable/emitter/references/property.js
+++ test/common/observable/emitter/references/property.js
@@ -5,31 +5,24 @@ describe('property', function () {
   it('should add a nested listener', function () {
     var a = new Observable()
     var b = new Observable()
+    var c = new Observable()
     // var aEvent = new Event()
     a.set({
-      ChildConstructor: 'Constructor'
-    })
-    var nestedConstructor = new Observable()
-
-    var nested = new Observable({
-      ChildConstructor: nestedConstructor.Constructor
-    })
-    b.set({
       properties: {
-        nested: nested
+        c: c
       }
     })
-
+    var nested = new Observable()
     a.set({
       c: { val: true }
     })
-
+    console.log(a.c)
     b.set({
       nested: { c: a.c }
     })
+    console.log(b.nested)
+
     expect(b.nested.c).has.property('_input').which.equals(a.c)
   })
-
-
 
 })
