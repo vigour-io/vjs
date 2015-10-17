89880eed5e3009ccffeb0fbb671002a2467844bf
Jim de Beer <jim@vigour.io>
2015-10-17 09:14:07 +0200

    fixed bug in useval closes #195

--- test/common/observable/emitter/references/property.js
+++ test/common/observable/emitter/references/property.js
@@ -1,28 +1,20 @@
 describe('property', function () {
   var Observable = require('../../../../../lib/observable/')
-  // var Event = require('../../../../../lib/event')
-
   it('should add a nested listener', function () {
     var a = new Observable()
     var b = new Observable()
     var c = new Observable()
-    // var aEvent = new Event()
     a.set({
       properties: {
         c: c
       }
     })
-    var nested = new Observable()
     a.set({
       c: { val: true }
     })
-    console.log(a.c)
     b.set({
       nested: { c: a.c }
     })
-    console.log(b.nested)
-
     expect(b.nested.c).has.property('_input').which.equals(a.c)
   })
-
 })
