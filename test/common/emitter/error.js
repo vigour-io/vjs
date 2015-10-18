ba14d00646dded0ce10b3981c14ea4121a2863d7
Jim de Beer <jim@vigour.io>
2015-10-19 00:29:57 +0200

    error emitter creates errors when passed a string, - added more error emitter tests

--- test/common/emitter/error.js
+++ test/common/emitter/error.js
@@ -1,30 +1,58 @@
 describe('error emitter', function () {
   var ErrorEmitter = require('../../../lib/emitter/error')
-  var error = new ErrorEmitter()
   var Event = require('../../../lib/event')
 
-  it('creates an error and fires it, data is single error', function () {
-    var event = new Event()
-    event.isTriggered = true
-    error.on(listener)
-    error.emit(void 0, event)
-    event.trigger()
-    function listener (data, event) {
-      this.data = data
-    }
-    expect(error.data).instanceof(Error)
+  describe('single emit', function () {
+    var emitter = new ErrorEmitter()
+    it('creates an error and fires it, data is single error', function () {
+      var event = new Event()
+      event.isTriggered = true
+      emitter.on(listener)
+      emitter.emit(void 0, event)
+      event.trigger()
+      function listener (data, event) {
+        this.data = data
+      }
+      expect(emitter.data).instanceof(Error)
+    })
   })
 
-  it('can fire for multiple error events', function () {
-    var event = new Event()
-    event.isTriggered = true
-    error.on(listener)
-    error.emit('a', event)
-    error.emit('b', event)
-    event.trigger()
+  describe('multiple emits', function () {
+    var emitter = new ErrorEmitter()
+    var path
+    var data
     function listener (data, event) {
       this.data = data
     }
-    expect(error.data).to.eql(["a", "b"])
+    it('fires for multiple error events', function () {
+      var event = new Event()
+      event.isTriggered = true
+      emitter.on(listener)
+      emitter.emit('a', event)
+      emitter.emit('b', event)
+      event.trigger()
+      data = emitter.data
+      path = emitter.path.join('.')
+    })
+
+    it('returns an array as data', function () {
+      expect(data).instanceof(Array)
+    })
+
+    it('has field [0]', function () {
+      expect(data[0]).instanceof(Error)
+    })
+
+    it('[0] has the correct message', function () {
+      expect(data[0]).property('message').which.equals(path + ' a')
+    })
+
+    it('has field [1]', function () {
+      expect(data[1]).instanceof(Error)
+    })
+
+    it('[1] has the correct message', function () {
+      expect(data[1]).property('message').which.equals(path + ' b')
+    })
   })
 })
