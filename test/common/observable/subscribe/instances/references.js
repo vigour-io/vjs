7cb4702fed626c293d32bf22657670d335061e96
Youri Daamen <me@youz.io>
2015-10-19 19:24:29 +0200

    reference testing

--- test/common/observable/subscribe/instances/references.js
+++ test/common/observable/subscribe/instances/references.js
@@ -60,6 +60,15 @@ describe('multiple instances with different references', function () {
   })
 
   it('update one reference, fires correct for each instance', function () {
+    console.log('----a')
+    content.a.title.val = 'aUpdatedTitle'
+    expect(count).equals(1)
+    expect(origins).contains('aUpdatedTitle')
+    expect(targets).contains('one')
+  })
+
+  it('update one reference, fires correct for each instance', function () {
+    console.log('----b')
     content.b.title.val = 'bUpdatedTitle'
     expect(count).equals(1)
     expect(origins).contains('bUpdatedTitle')
@@ -67,6 +76,7 @@ describe('multiple instances with different references', function () {
   })
 
   it('update another reference, fires correct for each instance', function () {
+    console.log('----c')
     content.c.title.val = 'cUpdatedTitle'
     expect(count).equals(1)
     expect(origins).contains('cUpdatedTitle')
