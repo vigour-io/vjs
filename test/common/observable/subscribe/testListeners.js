c131a476e4df70a180a80e76463d1f2df1a0d371
Youri Daamen <me@youz.io>
2015-10-19 15:54:57 +0200

    added files

---
+++ test/common/observable/subscribe/testListeners.js
@@ -0,0 +1,59 @@
+var info = require('../../../../lib/observable/subscribe/emitter/info')
+var getId = info.getId
+var getLateral = info.getLateral
+var getDepth = info.getDepth
+
+module.exports = function testListeners(subscribtion) {
+  var hash = subscribtion.key
+  var listeners = []
+  listeners.numberOf = function (value) {
+    var count = 0
+    for (var i = listeners.length - 1; i >= 0; i--) {
+      if (listeners[i] === value) {
+        count++
+      }
+    }
+    return count
+  }
+  var ids = []
+  if (subscribtion.listensOnAttach) {
+    subscribtion.listensOnAttach.each((property) => {
+      if (property.key === 'data') {
+        property.attach.each((prop) => {
+          listeners.push(property.key)
+          let info = prop[3]
+          let id = getId(info)
+          console.info(hash, '-', property.key, ':', info, '- id:', getId(info), '- lateral:', getLateral(info), '- depth:', getDepth(info))
+          expect(typeof info).equals('number')
+          expect(id).ok
+          expect(ids).not.contains(id)
+          ids.push(id)
+        })
+      }
+      if (property.key === 'property') {
+        property.attach.each((prop) => {
+          listeners.push(property.key)
+          let info = prop[3]
+          console.info(hash, '-', property.key, ':', info, '- id:', getId(info), '- lateral:', getLateral(info), '- depth:', getDepth(info))
+        })
+      }
+      if (property.key === 'parentEmitter') {
+        property.attach.each((prop) => {
+          listeners.push(property.key)
+          let info = prop[3]
+          console.info(hash, '-', property.key, ':', info, '- id:', getId(info), '- lateral:', getLateral(info), '- depth:', getDepth(info))
+        })
+      }
+      if (property.key === 'reference') {
+        property.attach.each((prop) => {
+          listeners.push(property.key)
+          let info = prop[3]
+          console.info(hash, '-', property.key, ':', info, '- id:', getId(info), '- lateral:', getLateral(info), '- depth:', getDepth(info))
+          expect(typeof info).equals('number')
+        })
+      }
+    })
+  }
+  console.info('--listeners:', listeners)
+  return listeners
+}
