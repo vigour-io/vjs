f5b5454fb46fe84e77e37e5175ed8d46ffbd6de0
Jim de Beer <jim@vigour.io>
2015-10-22 15:17:29 +0200

    added more tests for condition, fixed a bug in condition use resolved parent instead of normal

--- test/common/observable/emitter/condition/instances/index.js
+++ test/common/observable/emitter/condition/instances/index.js
@@ -20,48 +20,9 @@ describe('instances', function () {
     var b = new a.Constructor()
     a.val = 'a change!'
   })
-})
-
-describe('context', function () {
-  var Observable = require('../../../../../../lib/observable')
-  it('fires condition trigger', function (done) {
-    var cnt = 0
-    var dataCnt = 0
-    var fired = []
-    var a = new Observable({
-      key: 'a',
-      time: 10,
-      on: {
-        data: {
-          condition: function (data, done, event) {
-            setTimeout(() => done(), this.time.val)
-          },
-          val: function (data) {
-            fired.push(this.path[0])
-            cnt++
-            if(data === 'a change!') {
-              dataCnt++
-            }
-            if (cnt === 5 && dataCnt === 3) {
-
-              // expect(fired).to.deep.eql(['b', 'c', 'a', 'b', 'c'])
-              done()
-            }
-          }
-        }
-      }
-    })
-    // behaviour now is fire for each -- im fix operator first
-    // then we have batch
-    // think about the inputs what to do with them -- context will become a nightmare -- context condition is only from the original
-    // -- last things are fired with wrong time i geuss (the a time)
-    var b = new a.Constructor({time: 200, key: 'b'})
-    var c = new b.Constructor({time: 30, key: 'c'})
-    a.val = 'a change!'
-  })
 
   describe('references', function () {
-
+    var Observable = require('../../../../../../lib/observable')
     it('should fire conditions over references over instances', function (done) {
       var b = new Observable({
         val: a
@@ -83,53 +44,7 @@ describe('context', function () {
     })
   })
 
-  describe('ChildConstructor', function () {
-    it('should fire condition over ChildConstructors', function (done) {
-      var b = new Observable({
-        on: {
-          data: {
-            condition (val) {
-              expect(val).equals('fires')
-              expect(this.path[0]).equals('d')
-              done()
-            }
-          }
-        }
-      })
-      var c = new Observable({
-        ChildConstructor: b.Constructor
-      })
-      var d = new c.Constructor({
-        key: 'd',
-        field: 'fires'
-      })
-    })
+  require('./childconstructor')
+  require('./context')
 
-    it('should fire condition over ChildConstructors over nested fields', function (done) {
-      var results = []
-      var b = new Observable({
-        on: {
-          data: {
-            condition (val, next) {
-              // why does this not fire ? add test for without condition
-              results.push(this.path.join('.'))
-            }
-          }
-        },
-        ChildConstructor: 'Constructor'
-      })
-      var c = new Observable({
-        ChildConstructor: b.Constructor
-      })
-      var d = new c.Constructor({
-        key: 'd',
-        field: {
-          nestedField: 'start' //should fire for start as well!!!
-        }
-      })
-      d.field.nestedField.val = 'fire'
-      expect(results).to.deep
-        .equal(['d.field', 'd.field.nestedField', 'd.field.nestedField'])
-    })
-  })
 })
