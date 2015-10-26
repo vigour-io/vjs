7ba400d4a511f769a8bd5d080719c9a52d3d9533
Youri Daamen <me@youz.io>
2015-10-26 18:07:03 +0100

    added new files

--- test/common/util/safelySet.js
+++ test/common/util/safelySet.js
@@ -25,23 +25,34 @@ describe('safely set on Base', function () {
 describe('safely set on Observable', function () {
   it('should store the reserved fields elsewhere', function () {
     var test = new Observable()
+    console.clear()
     test.set({
-      set: 'test',
-      get: 'yoyo',
-      setKey: 'funtimes',
-      setKeyInternal: {
-        add: 12
-      },
-      nerdje: {
-        set: {
-          get: {
-            properties: {
-              blups: true
-            }
-          }
-        }
-      },
-      on: false
+      // set: 'test',
+      // get: 'yoyo',
+      // setKey: 'funtimes',
+      // setKeyInternal: {
+      //   add: 12
+      // },
+      // nerdje: {
+      //   set: {
+      //     get: {
+      //       properties: {
+      //         blups: true
+      //       }
+      //     }
+      //   }
+      // },
+      // parent:true,
+      path:true,
+      define: false
     }, void 0, void 0, 'randomPrefix_')
+    // expect(test.randomPrefix_set).ok
+    // expect(test.randomPrefix_get).ok
+    // expect(test.randomPrefix_setKey).ok
+    // expect(test.randomPrefix_on).ok
+    // expect(test.randomPrefix_setKeyInternal).ok
+    // expect(test.nerdje).ok
+    // expect(test.nerdje.randomPrefix_set.randomPrefix_get.randomPrefix_properties.blups).ok
+    expect(test.randomPrefix_path).ok
   })
 })
