bb38f564d64aeb9756918159fb57fabbbcf43428
Jim de Beer <jim@vigour.io>
2015-10-31 12:11:12 +0100

    bloks instances

--- test/common/observable/emitter/overwrites/input.js
+++ test/common/observable/emitter/overwrites/input.js
@@ -1,7 +1,7 @@
 'use strict'
 describe('input', function () {
   var Observable = require('../../../../../lib/observable')
-  it('instance, block update on instance', function () {
+  xit('instance, block update on instance', function () {
     var cnt = 0
     var a = new Observable({
       key: 'a',
@@ -21,9 +21,28 @@ describe('input', function () {
     expect(cnt).to.equal(1)
   })
 
+  it('instance, block update on instance, nested', function () {
+    var cnt = 0
+    var a = new Observable({
+      key: 'a',
+      on: {
+        data (data) {
+          console.log('fire:', data, this.path)
+          cnt++
+        }
+      }
+    })
+    var b = new a.Constructor({
+      key: 'b',
+      randomField: true
+    })
+    cnt = 0
+    a.set({ randomField: 'this is a!' })
+    expect(cnt).to.equal(1)
+  })
+
   // so why does context not have the correct data???
   // and what are the implications?
-
   // same for always emitting instances -- how much heavier does it make things
   xit('context, block update on context', function () {
     // console.clear()
