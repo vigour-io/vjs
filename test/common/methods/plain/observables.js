fc7be4f3f77dc4e73e71fe36851ed20ce6dc6d73
Jim de Beer <jim@vigour.io>
2015-11-23 00:14:38 +0100

    plain works on all nested base objects

--- test/common/methods/plain/observables.js
+++ test/common/methods/plain/observables.js
@@ -4,14 +4,16 @@ var plain = require('vigour-js/lib/methods/plain')
 describe('subclass.plain', function () {
   it('should work on instances of subclasses', function () {
     var Class = new Base({}).Constructor
+    Class.prototype.inject(plain)
     var Subclass = new Class({}).Constructor
-    Object.getPrototypeOf(Subclass.prototype).inject(plain)
+    // Object.getPrototypeOf(Subclass.prototype).inject(plain)
     var instance = new Subclass({
       prop: 'value'
     })
     var expected = {
       prop: 'value'
     }
+    console.log(instance.plain())
     expect(instance.plain()).to.eql(expected)
   })
 })
