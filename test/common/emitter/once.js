e1147813beebef452e0fa47e9eeec9d555e32b07
Jim de Beer <jim@vigour.io>
2015-11-25 11:57:40 +0100

    once on emitter

--- test/common/emitter/once.js
+++ test/common/emitter/once.js
@@ -4,7 +4,7 @@ describe('once', function () {
   var Emitter = require('../../../lib/emitter')
   var a = new Emitter()
   it('fires once, listener gets removed', function (done) {
-    a.once('data', function() {
+    a.once(function () {
       console.log('hey')
       // epxect(a.fn)
       done()
