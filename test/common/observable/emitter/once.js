90dd1024a136ad881e1ca877475def910e0909d1
Jim de Beer <jim@vigour.io>
2015-11-25 14:05:24 +0100

    once implementation

---
+++ test/common/observable/emitter/once.js
@@ -0,0 +1,8 @@
+'use strict'
+
+describe('once', function () {
+  var Observable = require('../../../../lib/observable')
+  it('can fire functions once', function () {
+    console.error('----->')
+  })
+})
