30d9d05d50b2201f4897b945a1f875041767f6ee
Youri Daamen <me@youz.io>
2015-12-05 16:23:05 +0100

    debugging subscribe attach

--- test/common/observable/subscribe/reference/property.js
+++ test/common/observable/subscribe/reference/property.js
@@ -3,35 +3,35 @@ var Observable = require('../../../../../lib/observable')
 var testListeners = require('../testListeners')
 var count
 
-beforeEach(() => {
+beforeEach(function () {
   count = 0
 })
 
-describe('subscribing to own reference', () => {
+describe('subscribing to own reference', function () {
   var subscription
   var ref = new Observable()
   var a = new Observable(ref)
 
-  it('subcribes to field', () => {
+  it('subcribes to field', function () {
     subscription = a.subscribe(true, function (event, meta) {
       count++
     })
     expect(count).equals(0)
   })
 
-  it('added a data listener', () => {
+  it('added a data listener', function () {
     var listeners = testListeners(subscription)
     expect(listeners.length).equals(1)
     expect(listeners).contains('data')
   })
 
-  it('fires when updated', () => {
+  it('fires when updated', function () {
     ref.val = 1
     expect(count).equals(1)
   })
 })
 
-describe('subscribing to single existing field, existing reference', () => {
+describe('subscribing to single existing field, existing reference', function () {
   var subscription
   var a = new Observable({
     key: 'a',
@@ -48,85 +48,85 @@ describe('subscribing to single existing field, existing reference', () => {
     val: b
   })
 
-  it('subcribes to field', () => {
+  it('subcribes to field', function () {
     subscription = c.subscribe({
       aField: true
-    }, () => {
+    }, function () {
       count++
     })
     expect(count).equals(0)
   })
 
-  it('added property and reference listener on c', () => {
+  it('added property and reference listener on c', function () {
     var listeners = testListeners(c)
     expect(listeners.length).equal(2)
     expect(listeners).contains('property')
     expect(listeners).contains('reference')
   })
 
-  it('added property and reference listener on b', () => {
+  it('added property and reference listener on b', function () {
     var listeners = testListeners(b)
     expect(listeners.length).equal(2)
     expect(listeners).contains('property')
     expect(listeners).contains('reference')
   })
 
-  it('added no listeners on a', () => {
+  it('added no listeners on a', function () {
     var listeners = testListeners(a)
     expect(listeners.length).equal(0)
   })
 
-  it('added data listener on a.aField', () => {
+  it('added data listener on a.aField', function () {
     var listeners = testListeners(a.aField)
     expect(listeners.length).equal(1)
     expect(listeners).contains('data')
   })
 
-  it('fires when field is updated on referenced obj', () => {
+  it('fires when field is updated on referenced obj', function () {
     a.aField.val = 2
     expect(count).equals(1)
   })
 
-  it('fires when field is created on obj', () => {
+  it('fires when field is created on obj', function () {
     b.set({
       aField: true
     })
     expect(count).equals(1)
   })
 
-  it('added data listener on b.aField', () => {
+  it('added data listener on b.aField', function () {
     var listeners = testListeners(b.aField)
     expect(listeners.length).equal(1)
     expect(listeners).contains('data')
   })
 
-  it('removed data listener on a.aField', () => {
+  it('removed data listener on a.aField', function () {
     var listeners = testListeners(a.aField)
     expect(listeners.length).equal(0)
   })
 
-  it('removed property listener on b', () => {
+  it('removed property listener on b', function () {
     var listeners = testListeners(b)
     expect(listeners.length).equal(0)
   })
 
-  it('doesn\'t fire when field is updated on referenced obj', () => {
+  it('doesn\'t fire when field is updated on referenced obj', function () {
     a.aField.val = 3
     expect(count).equals(0)
   })
 
-  it('removing b.aField fires subscription', () => {
+  it('removing b.aField fires subscription', function () {
     b.aField.remove()
     expect(count).equals(1)
   })
 
-  it('added data listener on a.aField', () => {
+  it('added data listener on a.aField', function () {
     var listeners = testListeners(a.aField)
     expect(listeners.length).equal(1)
     expect(listeners).contains('data')
   })
 
-  it('added property on b', () => {
+  it('added property on b', function () {
     var listeners = testListeners(b)
     expect(listeners.length).equal(2)
     expect(listeners).contains('property')
@@ -134,105 +134,105 @@ describe('subscribing to single existing field, existing reference', () => {
   })
 })
 
-describe('subscribing to existing field, and non existing field, existing reference', () => {
+describe('subscribing to existing field, and non existing field, existing reference', function () {
   var subscription
   var a = new Observable({
     aField: 1
   })
   var b = new Observable(a)
 
-  it('subcribes to two fields', () => {
+  it('subcribes to two fields', function () {
     subscription = b.subscribe({
       aField: true,
       bField: true
-    }, () => {
+    }, function () {
       count++
     })
     expect(count).equals(0)
   })
 
-  it('added property and reference listener on b', () => {
+  it('added property and reference listener on b', function () {
     var listeners = testListeners(b)
     expect(listeners.length).equal(2)
     expect(listeners).contains('property')
     expect(listeners).contains('reference')
   })
 
-  it('added property and reference listener on a', () => {
+  it('added property and reference listener on a', function () {
     var listeners = testListeners(a)
     expect(listeners.length).equal(2)
     expect(listeners).contains('property')
     expect(listeners).contains('reference')
   })
 
-  it('added data listener on a.aField', () => {
+  it('added data listener on a.aField', function () {
     var listeners = testListeners(a.aField)
     expect(listeners.length).equal(1)
     expect(listeners).contains('data')
   })
 
-  it('fires when field is updated on referenced obj', () => {
+  it('fires when field is updated on referenced obj', function () {
     a.aField.val = 2
     expect(count).equals(1)
   })
 
-  it('fires when field is created on obj', () => {
+  it('fires when field is created on obj', function () {
     b.set({
       aField: true
     })
     expect(count).equals(1)
   })
 
-  it('added data listener on b.aField', () => {
+  it('added data listener on b.aField', function () {
     var listeners = testListeners(b.aField)
     expect(listeners.length).equal(1)
     expect(listeners).contains('data')
   })
 
-  it('removed data listener on a.aField', () => {
+  it('removed data listener on a.aField', function () {
     var listeners = testListeners(a.aField)
     expect(listeners.length).equal(0)
   })
 
-  it('keeps listeners on b', () => {
+  it('keeps listeners on b', function () {
     var listeners = testListeners(b)
     expect(listeners.length).equal(2)
     expect(listeners).contains('property')
     expect(listeners).contains('reference')
   })
 
-  it('keeps listeners on a', () => {
+  it('keeps listeners on a', function () {
     var listeners = testListeners(a)
     expect(listeners.length).equal(2)
     expect(listeners).contains('property')
     expect(listeners).contains('reference')
   })
 
-  it('doesn\'t fire when field is updated on referenced obj', () => {
+  it('doesn\'t fire when field is updated on referenced obj', function () {
     a.aField.val = 3
     expect(count).equals(0)
   })
 
-  it('fires when field is created on obj', () => {
+  it('fires when field is created on obj', function () {
     b.set({
       bField: true
     })
     expect(count).equals(1)
   })
 
-  it('added data listener on b.bField', () => {
+  it('added data listener on b.bField', function () {
     var listeners = testListeners(b.bField)
     expect(listeners.length).equal(1)
     expect(listeners).contains('data')
   })
 
-  it('removed property and reference listener on b', () => {
+  it('removed property and reference listener on b', function () {
     var listeners = testListeners(b)
     expect(listeners.length).equal(0)
   })
 })
 
-describe('subscribing to single existing field, existing reference, switch reference', () => {
+describe('subscribing to single existing field, existing reference, switch reference', function () {
   var subscription
   var a = new Observable({
     key: 'a',
@@ -247,39 +247,39 @@ describe('subscribing to single existing field, existing reference, switch refer
     val: a
   })
 
-  it('subcribes to field', () => {
+  it('subcribes to field', function () {
     subscription = b.subscribe({
       aField: true
-    }, () => {
+    }, function () {
       count++
     })
     expect(count).equals(0)
   })
 
-  it('added data listener on a.aField', () => {
+  it('added data listener on a.aField', function () {
     var listeners = testListeners(a.aField)
     expect(listeners.length).equal(1)
     expect(listeners).contains('data')
   })
 
-  it('added property on b', () => {
+  it('added property on b', function () {
     var listeners = testListeners(b)
     expect(listeners.length).equal(2)
     expect(listeners).contains('property')
     expect(listeners).contains('reference')
   })
 
-  it('changing the reference to c, fires the sub', () => {
+  it('changing the reference to c, fires the sub', function () {
     b.val = c
     expect(count).equals(1)
   })
 
-  it('removed listeners on a', () => {
+  it('removed listeners on a', function () {
     var listeners = testListeners(a)
     expect(listeners.length).equal(0)
   })
 
-  it('added data listener on c.aField', () => {
+  it('added data listener on c.aField', function () {
     var listeners = testListeners(c.aField)
     expect(listeners.length).equal(1)
     expect(listeners).contains('data')
