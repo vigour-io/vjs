da5ecefd8a961fa0346c3e75121e848ef4fa5b1c
Jim de Beer <jim@vigour.io>
2015-12-07 17:03:04 +0100

    fixed condition test

--- test/common/observable/emitter/condition/instances/index.js
+++ test/common/observable/emitter/condition/instances/index.js
@@ -1,7 +1,7 @@
 'use strict'
 describe('instances', function () {
   var Observable = require('../../../../../../lib/observable')
-  
+
   it('fires condition trigger', function (done) {
     var cnt = 0
     var a = new Observable({
@@ -19,13 +19,14 @@ describe('instances', function () {
         }
       }
     })
-    var b = new a.Constructor()
+    var b = new a.Constructor() // eslint-disable-line
     a.val = 'a change!'
   })
 
   it('fires for inherited condition, new listener', function (done) {
     var count = 0
-    var conditionCount = 0
+    var passed = 0
+    console.clear()
     var A = new Observable({
       val: 10,
       on: {
@@ -38,16 +39,19 @@ describe('instances', function () {
       }
     })
     var a = new A.Constructor({
+      key: 'a',
       on: {
         data: {
           condition (data, cb, event) {
             count += 1
+            // gets here twice by the act of setting the condition this has to change
             setTimeout(cb, data)
           },
           val () {
-            conditionCount++
+            passed++
+            // only fires once for the event
             expect(count).to.equal(2)
-            if (count === 2 && conditionCount === 2) {
+            if (count === 2 && passed === 1) {
               done()
             }
           }
