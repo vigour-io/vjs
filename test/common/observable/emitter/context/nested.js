1296d343d30f927c35237a7c7e9d48b22e478bb5
Jim de Beer <jim@vigour.io>
2015-12-08 12:16:56 +0100

    moved test to seperate file

--- test/common/observable/emitter/context/nested.js
+++ test/common/observable/emitter/context/nested.js
@@ -73,33 +73,3 @@ describe('nested property listener on class fires on instance', function () {
     expect(aInstance.nested.d).to.not.equal(a.nested.d)
   })
 })
-
-describe('getter', function () {
-  var Observable = require('../../../../../lib/observable')
-  it('resets context after emitting', function () {
-    var paths = []
-    var datas = []
-    var d = new Observable({
-      trackInstances: true,
-      key: 'd',
-      field: {
-        on: {
-          data (data) {
-            datas.push(data)
-            paths.push(this.path.join('.'))
-          }
-        }
-      }
-    })
-    var e = new d.Constructor({
-      key: 'e'
-    })
-    var f = new e.Constructor({ //eslint-disable-line
-      key: 'f'
-    })
-    expect(e).to.have.property('_field')
-    e.set({ field: 'bla' })
-    expect(paths).to.deep.equal([ 'f.field', 'e.field' ])
-    expect(datas).to.deep.equal([ 'bla', 'bla' ])
-  })
-})
