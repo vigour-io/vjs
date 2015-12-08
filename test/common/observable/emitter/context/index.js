cb64b2853e57f595f14f6306235ebf2594a4ced9
Jim de Beer <jim@vigour.io>
2015-12-08 11:57:48 +0100

    cleanup

--- test/common/observable/emitter/context/index.js
+++ test/common/observable/emitter/context/index.js
@@ -29,6 +29,35 @@ describe('context', function () {
     }
   }
 
+  describe('getter', function () {
+    it('resets context after emitting', function () {
+      console.clear()
+      var paths = []
+      var datas = []
+      var d = new Observable({
+        key: 'd',
+        field: {
+          on: {
+            data (data) {
+              datas.push(data)
+              paths.push(this.path.join('.'))
+            }
+          }
+        }
+      })
+      var e = new d.Constructor({
+        key: 'e'
+      })
+      var f = new e.Constructor({ //eslint-disable-line
+        key: 'f'
+      })
+      expect(e).to.have.property('_field')
+      e.set({ field: 'bla' })
+      expect(paths).to.deep.equal([ 'e.field', 'f.field' ])
+      expect(datas).to.deep.equal([ 'bla', 'bla' ])
+    })
+  })
+
   describe('deep resolve test', function () {
     var test = contextObservable()
     it('should fire once for "blurf"', function () {
--- test/common/observable/emitter/data/context/property.js
+++ test/common/observable/emitter/data/context/property.js
@@ -56,31 +56,4 @@ describe('property', function () {
       { removed: [ 'field3' ] }
     ])
   })
-
-  it('resets context after emitting', function () {
-    console.clear()
-    var paths = []
-    var datas = []
-    var d = new Observable({
-      key: 'd',
-      field: {
-        on: {
-          data (data) {
-            datas.push(data)
-            paths.push(this.path.join('.'))
-          }
-        }
-      }
-    })
-    var e = new d.Constructor({
-      key: 'e'
-    })
-    var f = new e.Constructor({ //eslint-disable-line
-      key: 'f'
-    })
-    expect(e).to.have.property('_field')
-    e.set({ field: 'bla' })
-    expect(paths).to.deep.equal([ 'e.field', 'f.field' ])
-    expect(datas).to.deep.equal([ 'bla', 'bla' ])
-  })
 })
