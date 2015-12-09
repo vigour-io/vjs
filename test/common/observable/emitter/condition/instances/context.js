6e9673936d08e9e4f1a581b90ee26069f94d992b
Jim de Beer <jim@vigour.io>
2015-12-09 13:47:26 +0100

    first test passed

--- test/common/observable/emitter/condition/instances/context.js
+++ test/common/observable/emitter/condition/instances/context.js
@@ -2,40 +2,52 @@
 describe('context', function () {
   var Observable = require('../../../../../../lib/observable')
   it('fires condition trigger', function (done) {
-    var xcnt = 0
-    setInterval(function () {
-      if (xcnt < 1000) {
-        xcnt += 50
-        console.warn('tick:', xcnt)
+    var timer = 0
+    var interval = setInterval(function () {
+      if (timer < 1000) {
+        timer += 50
       }
     }, 50)
-    console.clear()
-    // var fired = []
+
+    var fired = {
+      a: { time: [], data: [] },
+      b: { time: [], data: [] }
+    }
+
     var a = new Observable({
       key: 'a',
       time: 100,
       on: {
         data: {
           condition (data, next, event) {
-            console.log('   fire condition in test:'.green.bold, '     ', event.stamp, this.path, this.time.val) // wrong context!
             setTimeout(() => next(), this.time.val)
           },
           val (data) {
+            var key = this.path[0]
+            fired[key].time.push(timer)
+            fired[key].data.push(data)
             console.log('fire listener (condition passed)'.green.bold.inverse, ' path: "' + this.path.join('.') + '"', data)
           }
         }
       }
     })
-    console.log('---------------------------------')
-    var b = new a.Constructor({ key: 'b', time: 300 })
-    console.log('uids:'.grey.bold, ' a:', a.uid, 'b:', b.uid)
-    // var c = new b.Constructor({time: 30, key: 'c'}) // eslint-disable-line
-    console.log('-----------a change--------------')
-    // a en b!
+
+    // fires for this as well make an overwrite settings
+    var b = new a.Constructor({ key: 'b', time: 300 }) // eslint-disable-line
     a.val = 'a change!'
+
+    setTimeout(function () {
+      expect(fired.a.time).to.deep.equal([100])
+      expect(fired.b.time).to.deep.equal([300, 300])
+      expect(fired.a.data).to.deep.equal(['a change!'])
+      expect(fired.b.data).to.deep.equal([{key: 'b', time: 300}, 'a change!'])
+      clearInterval(interval)
+      done()
+    }, 400)
   })
 
-  xit('fires condition trigger on resolved context', function (done) {
+  it('fires condition trigger on resolved context', function (done) {
+    console.clear()
     var a = new Observable({
       key: 'a',
       time: 10,
