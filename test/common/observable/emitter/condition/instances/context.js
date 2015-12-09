95c1e078d4f57bce05c86d40a04073e87c1a6339
Jim de Beer <jim@vigour.io>
2015-12-09 14:00:06 +0100

    added reference tests

--- test/common/observable/emitter/condition/instances/context.js
+++ test/common/observable/emitter/condition/instances/context.js
@@ -26,7 +26,6 @@ describe('context', function () {
             var key = this.path[0]
             fired[key].time.push(timer)
             fired[key].data.push(data)
-            console.log('fire listener (condition passed)'.green.bold.inverse, ' path: "' + this.path.join('.') + '"', data)
           }
         }
       }
@@ -37,30 +36,89 @@ describe('context', function () {
     a.val = 'a change!'
 
     setTimeout(function () {
-      expect(fired.a.time).to.deep.equal([100])
-      expect(fired.b.time).to.deep.equal([300, 300])
-      expect(fired.a.data).to.deep.equal(['a change!'])
-      expect(fired.b.data).to.deep.equal([{key: 'b', time: 300}, 'a change!'])
       clearInterval(interval)
+      expect(fired.a.time).to.deep.equal([ 100 ])
+      expect(fired.b.time).to.deep.equal([ 300, 300 ])
+      expect(fired.a.data).to.deep.equal([ 'a change!' ])
+      expect(fired.b.data).to.deep.equal([ { key: 'b', time: 300 }, 'a change!' ])
       done()
     }, 400)
   })
 
-  it('fires condition trigger on resolved context', function (done) {
-    console.clear()
+  it('fires condition trigger on resolved instance', function (done) {
+    var cnt = 0
     var a = new Observable({
       key: 'a',
-      time: 10,
       on: {
         data: {
           condition: function (data, next, event) {
+            cnt++
             next()
           }
         }
       }
     })
-    var b = new a.Constructor()
-    b.on(() => done())
+    var b = new a.Constructor({ key: 'b' }, false)
+    b.once(function () {
+      expect(cnt).to.equal(1)
+      done()
+    })
+    b.val = 'value'
+  })
+
+  it('fires base listeners', function (done) {
+    var cnt = 0
+    var timer
+    setTimeout(() => { timer = true }, 10)
+    var a = new Observable({
+      key: 'a',
+      on: {
+        data: {
+          condition: function (data, next, event) {
+            cnt++
+            expect(this.path[0]).to.equal('a')
+            setTimeout(next, 50)
+          }
+        }
+      }
+    })
+    var b = new Observable({
+      key: 'b',
+      val: a
+    })
+    b.once(function (data) {
+      expect(timer).to.equal(true)
+      expect(data).to.equal('value')
+      expect(cnt).to.equal(1)
+      done()
+    })
+    a.val = 'value'
+  })
+
+  it('fires over reference', function (done) {
+    var cnt = 0
+    var timer
+    setTimeout(() => { timer = true }, 10)
+    var b = new Observable({ key: 'b' })
+    var a = new Observable({
+      key: 'a',
+      val: b,
+      on: {
+        data: {
+          condition: function (data, next, event) {
+            cnt++
+            expect(this.path[0]).to.equal('a')
+            setTimeout(next, 50)
+          }
+        }
+      }
+    })
+    a.once(function (data) {
+      expect(timer).to.equal(true)
+      expect(data).to.equal('value')
+      expect(cnt).to.equal(1)
+      done()
+    })
     b.val = 'value'
   })
 })
