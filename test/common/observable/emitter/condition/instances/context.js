6ce29525ba25cb3c4f508a3ba770dac81381680c
Jim de Beer <jim@vigour.io>
2015-12-09 13:09:38 +0100

    fixed condition, fixed event deleteion

--- test/common/observable/emitter/condition/instances/context.js
+++ test/common/observable/emitter/condition/instances/context.js
@@ -2,47 +2,33 @@
 describe('context', function () {
   var Observable = require('../../../../../../lib/observable')
   it('fires condition trigger', function (done) {
-    var cnt = 0
+    var xcnt = 0
     setInterval(function () {
-      if (cnt < 1000) {
-        cnt += 50
-        console.warn('tick:', cnt)
+      if (xcnt < 1000) {
+        xcnt += 50
+        console.warn('tick:', xcnt)
       }
-    },50)
+    }, 50)
     console.clear()
-    var cnt = 0
-    var dataCnt = 0
-    var fired = []
+    // var fired = []
     var a = new Observable({
       key: 'a',
       time: 100,
       on: {
         data: {
           condition (data, next, event) {
-            console.log('fuck yu!', this.path, this.time.val) // wrong context!
-            // context get switched around of course
+            console.log('   fire condition in test:'.green.bold, '     ', event.stamp, this.path, this.time.val) // wrong context!
             setTimeout(() => next(), this.time.val)
           },
           val (data) {
-            console.log(' ---> ghello do ')
-            console.log('  hello', data)
-            fired.push(this.path[0])
-            cnt++
-            if (data === 'a change!') {
-              dataCnt++
-            }
-            console.log('  ???', cnt, dataCnt, this.path)
-            // this should fire 3 times for each context!
-            if (cnt === 5 && dataCnt === 3) {
-              // expect(fired).to.deep.eql(['b', 'c', 'a', 'b', 'c'])
-              done()
-            }
+            console.log('fire listener (condition passed)'.green.bold.inverse, ' path: "' + this.path.join('.') + '"', data)
           }
         }
       }
     })
     console.log('---------------------------------')
     var b = new a.Constructor({ key: 'b', time: 300 })
+    console.log('uids:'.grey.bold, ' a:', a.uid, 'b:', b.uid)
     // var c = new b.Constructor({time: 30, key: 'c'}) // eslint-disable-line
     console.log('-----------a change--------------')
     // a en b!
