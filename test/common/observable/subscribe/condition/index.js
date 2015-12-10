317e0c9be96f789de61873af27ee107af47c546b
Youri Daamen <me@youz.io>
2015-12-10 17:52:22 +0100

    added condition uses

--- test/common/observable/subscribe/condition/index.js
+++ test/common/observable/subscribe/condition/index.js
@@ -9,125 +9,125 @@ beforeEach(function () {
   countTwo = 0
 })
 
-// describe('simple condition', function () {
-//   var obs = new Observable({
-//     nested1: {
-//       nest: {
-//         title: 'foo'
-//       },
-//       trouble: false
-//     },
-//     nested2: {
-//       subtitle: 'bar'
-//     },
-//     nested3: {
-//       subtitle: 'funk'
-//     }
-//   })
+describe('simple condition', function () {
+  var obs = new Observable({
+    nested1: {
+      nest: {
+        title: 'foo'
+      },
+      trouble: false
+    },
+    nested2: {
+      subtitle: 'bar'
+    },
+    nested3: {
+      subtitle: 'funk'
+    }
+  })
 
-//   it('fired once', function () {
-//     obs.subscribe({
-//       nested1: {
-//         $condition: {
-//           nest: {
-//             title: true
-//           },
-//           trouble(trouble) {
-//             return trouble.val === false
-//           }
-//         }
-//       }
-//     }, function (data) {
-//       countOne++
-//     }).run()
+  it('fired once', function () {
+    obs.subscribe({
+      nested1: {
+        $condition: {
+          nest: {
+            title: true
+          },
+          trouble(trouble) {
+            return trouble.val === false
+          }
+        }
+      }
+    }, function (data) {
+      countOne++
+    }).run()
 
-//     expect(countOne).equals(1)
-//   })
+    expect(countOne).equals(1)
+  })
 
-//   it('updating condition title doesnt fire subscription', function () {
-//     obs.nested1.nest.title.val = 'foo'
-//     expect(countOne).equals(0)
-//   })
+  it('updating condition title doesnt fire subscription', function () {
+    obs.nested1.nest.title.val = 'foo'
+    expect(countOne).equals(0)
+  })
 
-//   it('updating nested1 fires subscription', function () {
-//     obs.nested1.val = 'foo'
-//     expect(countOne).equals(1)
-//   })
+  it('updating nested1 fires subscription', function () {
+    obs.nested1.val = 'foo'
+    expect(countOne).equals(1)
+  })
 
-//   it('invalidating condition removes listener on nested1', function () {
-//     obs.nested1.trouble.val = true
-//     obs.nested1.val = 'bar'
-//     expect(countOne).equals(0)
-//   })
-// })
+  it('invalidating condition removes listener on nested1', function () {
+    obs.nested1.trouble.val = true
+    obs.nested1.val = 'bar'
+    expect(countOne).equals(0)
+  })
+})
 
-// describe('simple condition using any', function () {
-//   var obs = new Observable({
-//     nested1: {
-//       nest: {
-//         title: 'foo'
-//       },
-//       trouble: false
-//     },
-//     nested2: {
-//       subtitle: 'bar'
-//     },
-//     nested3: {
-//       subtitle: 'funk'
-//     }
-//   })
+describe('simple condition using any', function () {
+  var obs = new Observable({
+    nested1: {
+      nest: {
+        title: 'foo'
+      },
+      trouble: false
+    },
+    nested2: {
+      subtitle: 'bar'
+    },
+    nested3: {
+      subtitle: 'funk'
+    }
+  })
 
-//   it('fired twice', function () {
-//     obs.subscribe({
-//       $any: {
-//         $condition: {
-//           subtitle: true
-//         }
-//       }
-//     }, function (data) {
-//       countOne++
-//     }).run()
+  it('fired twice', function () {
+    obs.subscribe({
+      $any: {
+        $condition: {
+          subtitle: true
+        }
+      }
+    }, function (data) {
+      countOne++
+    }).run()
 
-//     expect(countOne).equals(2)
-//   })
-// })
+    expect(countOne).equals(2)
+  })
+})
 
-// describe('multiple conditions using any', function () {
-//   var obs = new Observable({
-//     nested1: {
-//       nest: {
-//         title: 'foo'
-//       },
-//       trouble: false
-//     },
-//     nested2: {
-//       subtitle: 'bar'
-//     },
-//     nested3: {
-//       subtitle: 'funk'
-//     }
-//   })
+describe('multiple conditions using any', function () {
+  var obs = new Observable({
+    nested1: {
+      nest: {
+        title: 'foo'
+      },
+      trouble: false
+    },
+    nested2: {
+      subtitle: 'bar'
+    },
+    nested3: {
+      subtitle: 'funk'
+    }
+  })
 
-//   it('fired trice', function () {
-//     obs.subscribe({
-//       $any: {
-//         $condition: [{
-//           subtitle: true
-//         }, {
-//           nest: {
-//             title: true
-//           }
-//         }]
-//       }
-//     }, function (data) {
-//       countOne++
-//     }).run()
+  xit('fired trice', function () {
+    obs.subscribe({
+      $any: {
+        $condition: [{
+          subtitle: true
+        }, {
+          nest: {
+            title: true
+          }
+        }]
+      }
+    }, function (data) {
+      countOne++
+    }).run()
 
-//     expect(countOne).equals(3)
-//   })
-// })
+    expect(countOne).equals(3)
+  })
+})
 
-describe('multiple conditions using upward', function () {
+describe('condition using upward', function () {
   var title
   var obs = new Observable({
     key:'obs',
@@ -172,13 +172,54 @@ describe('multiple conditions using upward', function () {
 
   it('when adding it closer, subscription finds closer', function () {
     obs.content.nested.show.set({
-      title:'Murder was the case'
+      title: 'Murder was the case'
     })
 
     obs.content.show.set({
-      title:'Not this one'
+      title: 'Not this one'
     })
     expect(title).equals('Murder was the case')
+    expect(countOne).equals(1)
+  })
+})
+
+describe('using parent inside of condition, upwards', function () {
+  var found
+  var obs = new Observable({
+    key: 'obs',
+    title: 'fun',
+    content: {
+      nested: {
+        nest: {
+          field: {}
+        }
+      }
+    }
+  })
+
+  it('finds observable with a parent with a title', function () {
+    obs.content.nested.nest.field.subscribe({
+      $upward: {
+        $condition: {
+          parent: {
+            title: true
+          }
+        }
+      }
+    }, function (data) {
+      found = data[data.length - 1].origin.key
+      countOne++
+    }).run()
+
+    expect(found).equals('content')
+    expect(countOne).equals(1)
+  })
+
+  it('when adding it closer, subscription finds closer', function () {
+    obs.content.set({
+      title: 'Not this one'
+    })
+    expect(found).equals('nested')
     expect(countOne).equals(1)
   })
 })
