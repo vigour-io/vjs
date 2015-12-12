68f9bebcfbcddb7ac83fdd1cd77182c9121da1e4
Jim de Beer <jim@vigour.io>
2015-12-13 00:20:51 +0100

    update perf tests

--- test/performance/util/index.js
+++ test/performance/util/index.js
@@ -1,14 +1,13 @@
 'use strict'
 describe('util test', function () {
-  var perf = require('chai-performance')
-  perf.debug = true
-  chai.use(perf)
-  // this is buggy shit
   var isNumberLike = require('../../../lib/util/is/numberlike')
+  var perf = require('chai-performance')
+  perf.log = true
+  chai.use(perf)
 
   it('isNumberLike vs lodash isNumber', function (done) {
     this.timeout(50e3)
-    var amount = 1e4
+    var amount = 1e3
     var isNumber = require('lodash/lang/isNumber')
 
     expect(function () {
@@ -19,7 +18,7 @@ describe('util test', function () {
         isNumberLike(i)
       }
     }).performance({
-      loop: 1e4,
+      loop: 1e3,
       method () {
         for (var i = 0; i < amount; i++) {
           isNumber('a' + i)
