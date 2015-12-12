201bc390b96af7ff51f612985da027bf2fc8d9ce
Jim de Beer <jim@vigour.io>
2015-12-12 19:04:00 +0100

    perf tests

--- test/performance/util/index.js
+++ test/performance/util/index.js
@@ -1,12 +1,14 @@
 'use strict'
 describe('util test', function () {
+  var perf = require('chai-performance')
+  perf.debug = true
+  chai.use(perf)
   // this is buggy shit
-  var util = require('../../../lib/util')
   var isNumberLike = require('../../../lib/util/is/numberlike')
 
   it('isNumberLike vs lodash isNumber', function (done) {
     this.timeout(50e3)
-    var amount = 1e6
+    var amount = 1e4
     var isNumber = require('lodash/lang/isNumber')
 
     expect(function () {
@@ -17,37 +19,14 @@ describe('util test', function () {
         isNumberLike(i)
       }
     }).performance({
-      margin: 3,
-      method: function () {
+      loop: 1e4,
+      method () {
         for (var i = 0; i < amount; i++) {
           isNumber('a' + i)
         }
         for (i = 0; i < amount; i++) {
           isNumber(i)
         }
-      }
-    }, done)
-  })
-
-  it('convertToArray vs Array.prototype.slice', function (done) {
-    this.timeout(50e3)
-    var convertToArray = util.convertToArray
-    var slice = Array.prototype.slice
-    var amount = 1e6
-
-    expect(function () {
-      function fn () {
-        convertToArray(arguments)
-      }
-      for (var i = 0; i < amount; i++) {
-        fn(1, 2, 3, 4, 5, 6)
-      }
-    }).performance(function () {
-      function fn2 () {
-        slice.apply(arguments)
-      }
-      for (var i = 0; i < amount; i++) {
-        fn2(1, 2, 3, 4, 5, 6)
       }
     }, done)
   })
