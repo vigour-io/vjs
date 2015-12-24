5723542b8d2a9472fe8f6ed5047ef9f91b75263c
Jim de Beer <jim@vigour.io>
2015-12-24 18:54:28 +0100

    added more tests

--- test/performance/observable/subscribe/index.js
+++ test/performance/observable/subscribe/index.js
@@ -9,22 +9,8 @@ describe('Subscribe', function () {
   var amount = 1000
   var arr
 
-  function observable () {
-    arr = []
-    for (var i = 0; i < amount; i++) {
-      var a = new Observable({ //eslint-disable-line
-        b: true
-      })
-      a.subscribe({
-        b: true
-      })
-      arr.push(a)
-    }
-  }
-
-  it('creating observables add listeners and fire on existing field (' + amount + ')', function (done) {
-    this.timeout(50e3)
-    expect(function () {
+  describe('Observable default emitters (baseline)', function () {
+    function baseline () {
       arr = []
       for (var i = 0; i < amount; i++) {
         var a = new Observable({ //eslint-disable-line
@@ -38,61 +24,109 @@ describe('Subscribe', function () {
         a.val = i
         arr.push(a)
       }
-    }).performance({
-      loop: 10,
-      time: 100
-    }, done)
+    }
+
+    it('creating observables add listeners and fire on existing field (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(baseline).performance({
+        loop: 10,
+        time: 100
+      }, done)
+    })
+
+    it('firing listeners on observables (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(function () {
+        var len = arr.length
+        for (let i = 0; i < len; i++) {
+          arr[i].b.val = i * 2
+        }
+      }).performance({
+        loop: 10,
+        time: 100,
+        before: baseline
+      }, done)
+    })
+
+    it('creating observables add listeners using context (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      var thing = new Observable({
+        b: true,
+        on: { data () {} }
+      })
+      var Thing = thing.Constructor
+      expect(function () {
+        arr = []
+        for (var i = 0; i < amount; i++) {
+          var a = new Thing({
+            val: i
+          })
+          arr.push(a)
+        }
+      }).performance({
+        loop: 10,
+        time: 100
+      }, done)
+    })
   })
 
-  it('creating observables add listeners using context (' + amount + ')', function (done) {
-    this.timeout(50e3)
-    var thing = new Observable({
-      b: true,
-      on: { data () {} }
-    })
-    var Thing = thing.Constructor
-    expect(function () {
+  describe('Existing field', function () {
+    function observable () {
       arr = []
       for (var i = 0; i < amount; i++) {
-        var a = new Thing({
-          val: i
+        var a = new Observable({ //eslint-disable-line
+          b: true
         })
+        a.subscribe({
+          b: true
+        }, function () {})
         arr.push(a)
       }
-    }).performance({
-      loop: 10,
-      time: 100
-    }, done)
-  })
+    }
 
-  it('creating observables and subscribing on existing field (' + amount + ')', function (done) {
-    this.timeout(50e3)
-    expect(observable).performance({
-      loop: 10,
-      time: 100
-    }, done)
-  })
+    it('creating observables and subscribing on existing field (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(observable).performance({
+        loop: 10,
+        time: 100
+      }, done)
+    })
 
-  it('creating observables subscribing using context (' + amount + ')', function (done) {
-    this.timeout(50e3)
-    var thing = new Observable({
-      b: true
+    it('firing listeners on observables and subscribing on existing field (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(function () {
+        var len = arr.length
+        for (let i = 0; i < len; i++) {
+          arr[i].b.val = i * 2
+        }
+      }).performance({
+        loop: 10,
+        time: 100,
+        before: observable
+      }, done)
     })
-    thing.subscribe({
-      b: true
+
+    it('creating observables subscribing using context (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      var thing = new Observable({
+        b: true
+      })
+      thing.subscribe({
+        b: true
+      })
+      var Thing = thing.Constructor
+      expect(function () {
+        arr = []
+        for (var i = 0; i < amount; i++) {
+          var a = new Thing({
+            b: i
+          })
+          arr.push(a)
+        }
+      }).performance({
+        loop: 10,
+        time: 100
+      }, done)
     })
-    var Thing = thing.Constructor
-    expect(function () {
-      arr = []
-      for (var i = 0; i < amount; i++) {
-        var a = new Thing({
-          b: i
-        })
-        arr.push(a)
-      }
-    }).performance({
-      loop: 10,
-      time: 100
-    }, done)
   })
 })
