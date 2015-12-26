545aaf0443e9976a35ecb61eeae2f9bbadbf4009
Jim de Beer <jim@vigour.io>
2015-12-26 17:52:40 +0100

    added tests

--- test/performance/observable/subscribe/index.js
+++ test/performance/observable/subscribe/index.js
@@ -1,35 +1,52 @@
 'use strict'
 
 describe('Subscribe', function () {
+  var arr, target
   var perf = require('chai-performance')
   perf.log = true
   chai.use(perf)
   var Observable = require('../../../../lib/observable')
   // var Event = require('../../../lib/event')
   var amount = 1000
-  var arr
 
   describe('Observable default emitters (baseline)', function () {
     function baseline () {
       arr = []
       for (var i = 0; i < amount; i++) {
         var a = new Observable({ //eslint-disable-line
-          b: true,
           on: {
             data () {
 
             }
           }
         })
-        a.val = i
+        // a.val = i
         arr.push(a)
       }
     }
 
-    it('creating observables add listeners and fire on existing field (' + amount + ')', function (done) {
+    function baseline2 () {
+      arr = []
+      for (var i = 0; i < amount; i++) {
+        var a = new Observable()
+        a.on('data', function () {})
+        // a.val = i
+        arr.push(a)
+      }
+    }
+
+    it('creating observables with listeners using set object (' + amount + ')', function (done) {
       this.timeout(50e3)
       expect(baseline).performance({
-        loop: 100,
+        loop: 10,
+        time: 200
+      }, done)
+    })
+
+    it('creating observables with listeners using on (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(baseline2).performance({
+        loop: 10,
         time: 200
       }, done)
     })
@@ -42,13 +59,13 @@ describe('Subscribe', function () {
           arr[i].val = i * 2
         }
       }).performance({
-        loop: 100,
+        loop: 10,
         time: 100,
         before: baseline
       }, done)
     })
 
-    it('creating observables add listeners using context (' + amount + ')', function (done) {
+    it('creating observables add listeners on contextlevel , fire on creation (' + amount + ')', function (done) {
       this.timeout(50e3)
       var thing = new Observable({
         b: true,
@@ -64,8 +81,111 @@ describe('Subscribe', function () {
           arr.push(a)
         }
       }).performance({
-        loop: 100,
+        loop: 10,
         time: 100
+      }, done)
+    })
+
+    it('firing listeners on observables fire over context (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      var thing
+      expect(function () {
+        thing.val = 'a'
+      }).performance({
+        loop: 10,
+        time: 100,
+        before: function () {
+          arr = []
+          if (thing) {
+            thing.remove()
+          }
+          thing = new Observable({
+            b: true,
+            on: { data () {} }
+          })
+          var Thing = thing.Constructor
+          for (var i = 0; i < amount; i++) {
+            var a = new Thing()
+            arr.push(a)
+          }
+        }
+      }, done)
+    })
+
+    function onlistener () {
+      for (var i = 0; i < amount; i++) {
+        // make specifics for internal every field
+        // oemitData , onData etc -- this will imporve perf a lot im sure (way less checks)
+        arr[i].on('data', function () {})
+      }
+    }
+
+    it('add on listeners (basic data /w function) on existing emitters (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(onlistener).performance({
+        loop: 10,
+        time: 500,
+        before: baseline
+      }, done)
+    })
+
+    it('add on listeners (basic data /w function) create new emitters (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(onlistener).performance({
+        loop: 10,
+        time: 500,
+        before: function () {
+          arr = []
+          for (var i = 0; i < amount; i++) {
+            arr.push(new Observable())
+          }
+        }
+      }, done)
+    })
+
+    function createReferences () {
+      arr = []
+      // if (target) {
+        // target.remove()
+      // }
+      target = new Observable('a')
+      for (var i = 0; i < amount; i++) {
+        var a = new Observable({
+          val: target,
+          on: {
+            data () {}
+          }
+        })
+        arr.push(a)
+      }
+    }
+
+    it('create references (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(createReferences).performance({
+        loop: 10,
+        time: 500
+      }, done)
+    })
+
+    it('remove reference (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(function () {
+        target.remove()
+      }).performance({
+        loop: 10,
+        time: 500
+      }, done)
+    })
+
+    it('firing listeners on observables over references (' + amount + ')', function (done) {
+      this.timeout(50e3)
+      expect(function () {
+        target.val = 'b'
+      }).performance({
+        loop: 10,
+        time: 100,
+        before: createReferences
       }, done)
     })
   })
@@ -87,7 +207,7 @@ describe('Subscribe', function () {
     it('creating observables and subscribing on existing field (' + amount + ')', function (done) {
       this.timeout(50e3)
       expect(observable).performance({
-        loop: 100,
+        loop: 10,
         time: 100
       }, done)
     })
@@ -100,7 +220,7 @@ describe('Subscribe', function () {
           arr[i].b.val = i * 2
         }
       }).performance({
-        loop: 100,
+        loop: 10,
         time: 100,
         before: observable
       }, done)
@@ -124,7 +244,7 @@ describe('Subscribe', function () {
           arr.push(a)
         }
       }).performance({
-        loop: 100,
+        loop: 10,
         time: 100
       }, done)
     })
