164b6537a3bda89d21120bcd01f72b1b9db3696f
Jim de Beer <jim@vigour.io>
2015-12-27 21:00:52 +0100

    added references support

--- test/common/observable/emitter/references/index.js
+++ test/common/observable/emitter/references/index.js
@@ -31,7 +31,7 @@ describe('references', function () {
           // ORIGIN ON EVENT IS REMOVED BETTER (WAS NOT USED ANYWHERE)
           // var originkeyCnt = measure.a.val.origin[event.origin.key]
           // measure.a.val.origin[event.origin.key] = originkeyCnt ? (originkeyCnt + 1) : 1
-          console.log('yo yo yo!', this.path)
+          console.log('yo yo yo! FIRE THAT BITCH', this.path)
           var keyCnt = measure.a.val[this.key]
           measure.a.val.total += 1
           measure.a.val[this.key] = keyCnt ? (keyCnt + 1) : 1
@@ -64,77 +64,89 @@ describe('references', function () {
     // expect(measure.a.val.origin.aRef).msg('aRef origin').to.equal(2)
   })
 
-  // it('add change listener "second" on b', function () {
-  //   measure.b.second = {
-  //     total: 0,
-  //     origin: {}
-  //   }
-  //   b.val = {
-  //     on: {
-  //       data: {
-  //         second: function (data, event) {
-  //           var originkeyCnt = measure.b.second.origin[event.origin.key]
-  //           measure.b.second.origin[event.origin.key] = originkeyCnt ? (originkeyCnt + 1) : 1
-  //           var keyCnt = measure.b.second[this.key]
-  //           measure.b.second.total += 1
-  //           measure.b.second[this.key] = keyCnt ? (keyCnt + 1) : 1
-  //         }
-  //       }
-  //     }
-  //   }
-  //   expect(measure.a.val.a).msg('a context').to.equal(2)
-  //   expect(measure.a.val.b).msg('b context').to.equal(3)
-  //   expect(measure.a.val.total).to.equal(5)
-  //   expect(measure.b.second.total).to.equal(0)
-  //   expect(measure.a.val.origin.aRef).msg('aRef origin').to.equal(2)
-  // })
-  //
-  // it('add property "prop1" on a', function () {
-  //   a.val = {
-  //     prop1: true
-  //   }
-  //   expect(measure.a.val.a).msg('a context').to.equal(3)
-  //   expect(measure.a.val.b).msg('b context').to.equal(4)
-  //   expect(measure.a.val.total).to.equal(7)
-  //   expect(measure.b.second.b).msg('b context').to.equal(1)
-  //   expect(measure.b.second.total).to.equal(1)
-  // })
-  //
-  // it('change aRef', function () {
-  //   aRef.val = 'another change'
-  //   expect(measure.a.val.a).msg('a context (a val)').to.equal(4)
-  //   expect(measure.a.val.b).msg('b context (a val)').to.equal(5)
-  //   expect(measure.a.val.total).to.equal(9)
-  //   expect(measure.b.second.b).msg('b context (b second)').to.equal(2)
-  //   expect(measure.b.second.total).to.equal(2)
-  //   expect(measure.a.val.origin.aRef).msg('aRef origin (a val)').to.equal(4)
-  //   expect(measure.b.second.origin.aRef).msg('aRef origin )(b context)').to.equal(1)
-  // })
-  //
-  // it('create new aRef --> bRef', function () {
-  //   bRef = new aRef.Constructor({
-  //     key: 'bRef'
-  //   })
-  //   aRef.val = 'wow more change'
-  //   expect(measure.a.val.a).msg('a context (a val)').to.equal(5)
-  //   expect(measure.a.val.b).msg('b context (a val)').to.equal(6)
-  //   expect(measure.a.val.total).to.equal(11)
-  //   expect(measure.b.second.b).msg('b context (b second)').to.equal(3)
-  //   expect(measure.b.second.total).to.equal(3)
-  // })
-  //
-  // it('change bRef', function () {
-  //   bRef.val = 'im changeing bRef'
-  //   expect(measure.a.val.a).msg('a context (a val)').to.equal(5)
-  //   expect(measure.a.val.b).msg('b context (a val)').to.equal(6)
-  //   expect(measure.a.val.total).to.equal(11)
-  //   expect(measure.b.second.b).msg('b context (b second)').to.equal(3)
-  //   expect(measure.b.second.total).to.equal(3)
-  // })
-  //
+  it('add change listener "second" on b', function () {
+    measure.b.second = {
+      total: 0,
+      origin: {}
+    }
+    b.val = {
+      on: {
+        data: {
+          second: function (data, event) {
+            // var originkeyCnt = measure.b.second.origin[event.origin.key]
+            // measure.b.second.origin[event.origin.key] = originkeyCnt ? (originkeyCnt + 1) : 1
+            var keyCnt = measure.b.second[this.key]
+            measure.b.second.total += 1
+            measure.b.second[this.key] = keyCnt ? (keyCnt + 1) : 1
+          }
+        }
+      }
+    }
+    expect(measure.a.val.a).msg('a context').to.equal(2)
+    expect(measure.a.val.b).msg('b context').to.equal(3)
+    expect(measure.a.val.total).to.equal(5)
+    expect(measure.b.second.total).to.equal(0)
+    // expect(measure.a.val.origin.aRef).msg('aRef origin').to.equal(2)
+  })
+
+  it('add property "prop1" on a', function () {
+    a.val = {
+      prop1: true
+    }
+    expect(measure.a.val.a).msg('a context').to.equal(3)
+    expect(measure.a.val.b).msg('b context').to.equal(4)
+    expect(measure.a.val.total).to.equal(7)
+    expect(measure.b.second.b).msg('b context').to.equal(1)
+    expect(measure.b.second.total).to.equal(1)
+  })
+
+  it('change aRef', function () {
+    aRef.val = 'another change'
+    expect(measure.a.val.a).msg('a context (a val)').to.equal(4)
+    expect(measure.a.val.b).msg('b context (a val)').to.equal(5)
+    expect(measure.a.val.total).to.equal(9)
+    expect(measure.b.second.b).msg('b context (b second)').to.equal(2)
+    expect(measure.b.second.total).to.equal(2)
+    // expect(measure.a.val.origin.aRef).msg('aRef origin (a val)').to.equal(4)
+    // expect(measure.b.second.origin.aRef).msg('aRef origin )(b context)').to.equal(1)
+  })
+
+  it('create new aRef --> bRef', function () {
+    console.clear()
+    bRef = new aRef.Constructor({
+      // exlcude it when in instance!
+      key: 'bRef'
+    })
+    expect(measure.a.val.a).msg('a context (a val)').to.equal(4)
+
+    // fires for bref of course dirty balls
+    aRef.val = 'wow more change'
+    // should be 5? how 7??? one to many
+    // so whats happenin here?
+
+    expect(measure.a.val.a).msg('a context (a val)').to.equal(5)
+    // this is wrong should not fire on new listener thats lame
+
+    // expect(measure.a.val.b).msg('b context (a val)').to.equal(6)
+
+    // expect(measure.a.val.total).to.equal(11)
+
+    // expect(measure.b.second.b).msg('b context (b second)').to.equal(3)
+    // expect(measure.b.second.total).to.equal(3)
+  })
+
+  it('change bRef', function () {
+    bRef.val = 'im changeing bRef'
+    expect(measure.a.val.a).msg('a context (a val)').to.equal(5)
+    expect(measure.a.val.b).msg('b context (a val)').to.equal(6)
+    expect(measure.a.val.total).to.equal(11)
+    expect(measure.b.second.b).msg('b context (b second)').to.equal(3)
+    expect(measure.b.second.total).to.equal(3)
+  })
+
   // require('./unique')
 })
-//
+
 // describe('on reference, switching reference multiple times', function () {
 //   var count = 0
 //
