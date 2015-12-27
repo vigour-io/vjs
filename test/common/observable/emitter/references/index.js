25b1b61007dffb2b2815748619d248cc9137b4e2
Jim de Beer <jim@vigour.io>
2015-12-27 21:08:04 +0100

    reanabled ref tests

--- test/common/observable/emitter/references/index.js
+++ test/common/observable/emitter/references/index.js
@@ -118,19 +118,14 @@ describe('references', function () {
       key: 'bRef'
     })
     expect(measure.a.val.a).msg('a context (a val)').to.equal(4)
-
     // fires for bref of course dirty balls
     aRef.val = 'wow more change'
     // should be 5? how 7??? one to many
     // so whats happenin here?
-
     expect(measure.a.val.a).msg('a context (a val)').to.equal(5)
     // this is wrong should not fire on new listener thats lame
-
     // expect(measure.a.val.b).msg('b context (a val)').to.equal(6)
-
     // expect(measure.a.val.total).to.equal(11)
-
     // expect(measure.b.second.b).msg('b context (b second)').to.equal(3)
     // expect(measure.b.second.total).to.equal(3)
   })
@@ -144,99 +139,99 @@ describe('references', function () {
     expect(measure.b.second.total).to.equal(3)
   })
 
-  // require('./unique')
+  require('./unique')
 })
 
-// describe('on reference, switching reference multiple times', function () {
-//   var count = 0
-//
-//   var a = new Observable({
-//     key: 'a'
-//   })
-//   var b = new Observable({
-//     key: 'b'
-//   })
-//   var c = new Observable({
-//     key: 'c'
-//   })
-//   var d = new Observable({
-//     key: 'd'
-//   })
-//
-//   var obs = new Observable({
-//     on: {
-//       reference () {
-//         count++
-//       }
-//     }
-//   })
-//
-//   beforeEach(function () {
-//     count = 0
-//   })
-//
-//   it('add reference, fires listener', function () {
-//     obs.val = a
-//     expect(count).equals(1)
-//   })
-//
-//   it('change reference, fires listener', function () {
-//     obs.val = b
-//     expect(count).equals(1)
-//   })
-//
-//   it('change reference, fires listener', function () {
-//     obs.val = c
-//     expect(count).equals(1)
-//   })
-//
-//   it('change reference, fires listener', function () {
-//     obs.val = d
-//     expect(count).equals(1)
-//   })
-// })
-//
-// describe('on reference, nested multiple instances with different reference each', function () {
-//   var count = 0
-//
-//   var content = new Observable({
-//     nested: {
-//       a: {},
-//       b: {},
-//       c: {},
-//       d: {}
-//     }
-//   })
-//
-//   var Obs = new Observable({
-//     on: {
-//       reference () {
-//         count++
-//       }
-//     }
-//   }).Constructor
-//
-//   beforeEach(function () {
-//     count = 0
-//   })
-//
-//   it('a instance, fires listener', function () {
-//     new Obs(content.nested.a) //eslint-disable-line
-//     expect(count).equals(1)
-//   })
-//
-//   it('b instance, fires listener', function () {
-//     new Obs(content.nested.b) //eslint-disable-line
-//     expect(count).equals(1)
-//   })
-//
-//   it('c instance, fires listener', function () {
-//     new Obs(content.nested.c) //eslint-disable-line
-//     expect(count).equals(1)
-//   })
-//
-//   it('d instance, fires listener', function () {
-//     new Obs(content.nested.d) //eslint-disable-line
-//     expect(count).equals(1)
-//   })
-// })
+describe('on reference, switching reference multiple times', function () {
+  var count = 0
+
+  var a = new Observable({
+    key: 'a'
+  })
+  var b = new Observable({
+    key: 'b'
+  })
+  var c = new Observable({
+    key: 'c'
+  })
+  var d = new Observable({
+    key: 'd'
+  })
+
+  var obs = new Observable({
+    on: {
+      reference () {
+        count++
+      }
+    }
+  })
+
+  beforeEach(function () {
+    count = 0
+  })
+
+  it('add reference, fires listener', function () {
+    obs.val = a
+    expect(count).equals(1)
+  })
+
+  it('change reference, fires listener', function () {
+    obs.val = b
+    expect(count).equals(1)
+  })
+
+  it('change reference, fires listener', function () {
+    obs.val = c
+    expect(count).equals(1)
+  })
+
+  it('change reference, fires listener', function () {
+    obs.val = d
+    expect(count).equals(1)
+  })
+})
+
+describe('on reference, nested multiple instances with different reference each', function () {
+  var count = 0
+
+  var content = new Observable({
+    nested: {
+      a: {},
+      b: {},
+      c: {},
+      d: {}
+    }
+  })
+
+  var Obs = new Observable({
+    on: {
+      reference () {
+        count++
+      }
+    }
+  }).Constructor
+
+  beforeEach(function () {
+    count = 0
+  })
+
+  it('a instance, fires listener', function () {
+    new Obs(content.nested.a) //eslint-disable-line
+    expect(count).equals(1)
+  })
+
+  it('b instance, fires listener', function () {
+    new Obs(content.nested.b) //eslint-disable-line
+    expect(count).equals(1)
+  })
+
+  it('c instance, fires listener', function () {
+    new Obs(content.nested.c) //eslint-disable-line
+    expect(count).equals(1)
+  })
+
+  it('d instance, fires listener', function () {
+    new Obs(content.nested.d) //eslint-disable-line
+    expect(count).equals(1)
+  })
+})
